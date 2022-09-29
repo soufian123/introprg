a37a6f40cf9abfab4f5d38e79557719154cfd731
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-09-29 19:59:07 +0200

    Completat l'exercici en que em presento

---
+++ 00_05_presentemnos/EmPresento.java
@@ -0,0 +1,12 @@
+/* Programa que saluda al món */
+
+public class EmPresento {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola, sóc Soufian, la futura millor desenvolupadora del Món!");
+
+    }
+
+}
+
