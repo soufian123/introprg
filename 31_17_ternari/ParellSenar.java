e646b44911543ea2e70c847721c4ddf632e9c7a3
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-10-08 00:59:28 +0200

    ternari

--- 31_17_ternari/ParellSenar.java
+++ 31_17_ternari/ParellSenar.java
@@ -4,7 +4,7 @@
  * és parell o senar.
  * Si no s'especifica cap número, el resultat és indeterminat.
  */
-public class ternari {
+public class ParellSenar {
     public static void main(String[] args) {
         int numero = Integer.parseInt(args[0]);
         int resultat;
