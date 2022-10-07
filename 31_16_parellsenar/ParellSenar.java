f2859f5c4a004c82346d8f681ba75f7a884c928f
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-10-07 17:01:34 +0200

    Parells i senars

---
+++ 31_16_parellsenar/ParellSenar.java
@@ -0,0 +1,16 @@
+/*
+ * Programa que indica si el número proporcionat com a primer argument
+ * és parell o senar.
+ * Si no s'especifica cap número, el resultat és indeterminat.
+ */
+public class ParellSenar {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(args[0]);
+        if (numero % 2 == 0){
+            System.out.println("El número"+ numero +"és senar");
+        } else{
+            System.out.println("El número"+ numero +"és parell");
+        }
+    }
+}
+
