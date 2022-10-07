c1c67533ac443534c533ed6fdd9e6e6bf090feba
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-10-07 17:02:47 +0200

    Parells i senars  ∾ prgtest autocommit 31_16 #2

--- 31_16_parellsenar/ParellSenar.java
+++ 31_16_parellsenar/ParellSenar.java
@@ -7,9 +7,9 @@ public class ParellSenar {
     public static void main(String[] args) {
         int numero = Integer.parseInt(args[0]);
         if (numero % 2 == 0){
-            System.out.println("El número"+ numero +"és parell");
+            System.out.println("El número "+ numero +" és parell");
         } else{
-            System.out.println("El número"+ numero +"és senar");
+            System.out.println("El número "+ numero +" és senar");
         }
     }
 }
