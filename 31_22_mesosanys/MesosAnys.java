f7df7622f0f9cb94f71b6d95c90b1031b7606729
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-10-12 01:17:03 +0200

    Mesos i anys  ∾ prgtest autocommit 31_22 #3

--- 31_22_mesosanys/MesosAnys.java
+++ 31_22_mesosanys/MesosAnys.java
@@ -8,9 +8,16 @@ public class MesosAnys {
         int mes = Integer.parseInt(Entrada.readLine());
         System.out.println("Any?");
         int any = Integer.parseInt(Entrada.readLine());
-        
-        int mesAnterior = ( mes - 1);
+        if (mes == 12) {
+             int mesSeguent = (mesSeguent = (0));
+              } else {
+                       if (mes == 0) {
+                            int mesAnterior = ( mesAnterior = (12));
+                                    } else {
+
+       }}
+       int mesAnterior = ( mes - 1);
         int mesSeguent = (1 + mes);
         System.out.println("Mes anterior "+ mesAnterior + "/" +any+ " i mes següent " + mesSeguent + "/" +any);
-        }
+}
 }
