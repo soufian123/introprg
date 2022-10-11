6e05615685c7c203c46dfbb7b8be245779770430
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-10-11 23:53:52 +0200

    Els dies de la setmana

---
+++ DiaSetmana.java
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+    public class DiaSetmana {
+        public static void main(String[] args) {
+         int dia = Integer.parseInt(Entrada.readLine());
+         
+         String resultat;
+         
+        if (dia == 1) {
+
+          resultat = "Dilluns";
+
+             } else {
+
+               if (dia == 2) {
+
+                   resultat = "Dimarts";
+
+                      } else {
+
+                          if (dia == 3) {
+
+                            resultat = "Dimecres";
+
+                                  } else {
+
+                                       if (dia == 4) {
+    
+                                        resultat = "Dijous";
+
+                                               } else {
+                                               
+                                                       if (dia == 5) {
+    
+                                                            resultat = "Divendres";
+
+                                                                  } else {
+
+                                                                       if (dia == 6) {
+    
+                                                                            resultat = "Dissabte";
+
+                                                                                       } else {
+
+                                                                                                if (dia == 7) {
+    
+                                                                                                    resultat = "Diumenge";
+
+                                                                                                        } else {
+
+                                                                                                                resultat = "Error";
+
+
+
+
+            }}}}}}
+
+        }
+        System.out.println(resultat);
+    }
+}
