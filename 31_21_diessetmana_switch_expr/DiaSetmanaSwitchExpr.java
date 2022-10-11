26c49e2c81da2552c1f45b2ba3724e32776bc065
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-10-12 00:53:49 +0200

    dies de la setmana

---
+++ 31_21_diessetmana_switch_expr/DiaSetmanaSwitchExpr.java
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+    public class DiaSetmanaSwitchExpr {
+        public static void main(String[] args) {
+         int dia = Integer.parseInt(args[0]);
+         
+         String resultat;
+         
+         switch (dia) {
+         case 1: resultat = "Dilluns";
+                 break;
+         case 2: resultat = "Dimarts";
+                 break;
+         case 3: resultat = "Dimecres";
+                 break;
+         case 4: resultat = "Dijous";
+                 break;
+         case 5: resultat = "Divendres";
+                 break;
+         case 6: resultat = "Dissabte";
+                 break;
+         case 7: resultat = "Diumenge";
+                 break;
+         default: resultat = "Error";
+                 break;
+         
+         }
+       
+
+      
+        System.out.println(""+ resultat +"");
+    }
+}
