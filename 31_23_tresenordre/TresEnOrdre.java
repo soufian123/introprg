f47b693dec9d33f0b670230b8c5e09b54f61d481
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-10-14 16:32:31 +0200

    Tres en ordre

---
+++ 31_23_tresenordre/TresEnOrdre.java
@@ -0,0 +1,29 @@
+        /*
+         *Programa que ordena 3 nombres
+         *
+         */
+         
+
+public class TresEnOrdre {
+    public static void main(String[] args) {
+        System.out.println("Primer?");
+        int primer = Integer.parseInt(Entrada.readLine());
+           System.out.println("Segon?");
+        int segon = Integer.parseInt(Entrada.readLine());
+           System.out.println("Tercer?");
+        int tercer = Integer.parseInt(Entrada.readLine());
+        
+        if (primer<segon && segon<tercer) {
+             System.out.println(primer + ", " + segon + " i " + tercer);
+        } else {
+                if (tercer<primer && primer<segon) {
+                     System.out.println(tercer + ", " + primer + " i " + segon);
+                } else {
+                 System.out.println(segon + ", " + tercer + " i " + primer);
+                        
+                                        
+
+        }}
+    }
+}
+
