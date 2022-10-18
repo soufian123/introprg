aa6fbdb8f2deb98da8041aae421a7d5b87a0255a
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-10-18 17:18:05 +0200

    suma positius  ∾ prgtest autocommit 31_31 #4

--- 31_31_sumamaxtres/SumaPositius.java
+++ 31_31_sumamaxtres/SumaPositius.java
@@ -14,27 +14,31 @@ public class SumaPositius {
         // demanem valors
 
         // declarem la variable que contindrà els valors llegits
+
         int valor;
+
         // processem el primer valor
         System.out.println("Introdueix el primer valor");
         valor = Integer.parseInt(Entrada.readLine());
         if (valor >= 0) {
             suma = suma + valor;
-        }else {System.out.println("La suma és " + suma);}
+        }
+        
 
         // processem el segon valor
         System.out.println("Introdueix el segon valor");
         valor = Integer.parseInt(Entrada.readLine());
         if (valor >= 0) {
             suma = suma + valor;
-        }else {System.out.println("La suma és " + suma);}
+        } 
         
+
         // processem el tercer valor
         System.out.println("Introdueix el tercer valor");
         valor = Integer.parseInt(Entrada.readLine());
         if (valor >= 0) {
             suma = suma + valor;
-        }else {System.out.println("La suma és " + suma);}
-
+}
+        System.out.println("La suma és " + suma);
     }
 }
