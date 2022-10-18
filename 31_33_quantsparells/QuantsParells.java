0807031e10d2ed2c887e2a32e407739d7a3bb1b4
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-10-18 19:04:26 +0200

    Quants parells

---
+++ 31_33_quantsparells/QuantsParells.java
@@ -0,0 +1,16 @@
+/*
+*
+*un programa que vagi demanant enters positius i que, en el moment que 
+*rebi un de negatiu, mostri el nombre de valors parells introduïts i finalitzi l'execució.
+*
+*
+*/ 
+
+public class QuantsParells {
+    public static void main(String[] args) {
+    
+    
+        
+         
+
+
