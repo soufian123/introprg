632c8c1cdbe56d1b59871ad356302158e1babd6b
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-10-19 17:24:05 +0200

    Quants parells

--- 31_33_quantsparells/QuantsParells.java
+++ 31_33_quantsparells/QuantsParells.java
@@ -8,9 +8,30 @@
 
 public class QuantsParells {
     public static void main(String[] args) {
-    
-    
-        
+
+        int suma = 0;
+
+        int valor = 0;
+
+        while (valor >= 0) {
+
+            System.out.println("Introdueix valor");
+
+            valor = Integer.parseInt(Entrada.readLine());
+
+            if (valor % 2 == 0) {
+
+                suma = suma + valor;
+
+            }
+
+        }
+
+        System.out.println("La suma és " + suma);
+
+    }
+
+}
          
 
 
