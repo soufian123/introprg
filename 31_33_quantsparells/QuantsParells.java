585b2f51f2d380b52ae24ca2bcb211d4312f887b
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-10-19 18:54:16 +0200

    Quants parells  ∾ prgtest autocommit 31_33 #12

--- 31_33_quantsparells/QuantsParells.java
+++ 31_33_quantsparells/QuantsParells.java
@@ -19,7 +19,7 @@ public class QuantsParells {
 
             valor = Integer.parseInt(Entrada.readLine());
 
-            if (valor % 2 == 0) {
+            if (valor % 2 == 0 && valor >=0) {
             
                 numParells = numParells + 1;
         
