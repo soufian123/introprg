06218070933b39e3317adbfd9cecc575f5277768
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-10-19 19:10:01 +0200

    El lloro

---
+++ 31_34_lloro/Lloro.java
@@ -0,0 +1,22 @@
+/*
+ *
+ *Desenvolupa un programa que vagi demanant paraules i 
+ *les repeteixi fins que rebi una resposta en blanc o bé buida. 
+ *És a dir, la resposta sigui un return directament o bé només espais.
+ *
+*/
+
+public class Lloro {
+    public static void main(String[] args) {
+    
+    
+        String paraula;
+
+        paraula = Entrada.readLine();
+        while (paraula.equals(" ") && paraula.equals("")) {
+        System.out.println(" "+ paraula);
+        paraula = Entrada.readLine();
+        }
+        System.out.println("Adéu");
+    }
+}
