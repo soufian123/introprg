a4895013fbd4a910b960044c2faae6011acf980b
prgtest <moises.gomez@iesjoandaustria.org>
2022-10-20 16:01:53 +0200

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 31_34 #6

--- 31_34_lloro/Lloro.java
+++ 31_34_lloro/Lloro.java
@@ -13,7 +13,7 @@ public class Lloro {
         String paraula = Entrada.readLine();
         boolean empty=paraula.isEmpty();
 		boolean blank=paraula.isBlank();
-        while (paraula) {
+        while (empty==false && blank==false) {
                if (paraula.isEmpty()||paraula.isBlank()) {
                System.out.println("Adéu");
                }else{
