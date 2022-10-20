75a92e36e786f220f0b386f4f4ff145cba57d133
prgtest <moises.gomez@iesjoandaustria.org>
2022-10-20 15:33:07 +0200

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 31_34 #3

--- 31_34_lloro/Lloro.java
+++ 31_34_lloro/Lloro.java
@@ -11,7 +11,7 @@ public class Lloro {
     
         System.out.println("El lloro espera paraula:");
         String paraula = Entrada.readLine();
-        while (paraula.isBlank(  )) {
+        while (paraula.isEmpty(  )) {
                System.out.println("El lloro repeteix: "+ paraula);
                System.out.println("El lloro espera paraula:");
                paraula = Entrada.readLine();}
