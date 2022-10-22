32202baab6c0ea3977c921d258ecb7c9b70c9e93
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-10-22 13:32:53 +0200

    endevina nombre millorat  ∾ prgtest autocommit 31_40 #5

--- 31_40_endevina_cancel/EndevinaNombre.java
+++ 31_40_endevina_cancel/EndevinaNombre.java
@@ -22,7 +22,9 @@ public static void main (String[] args) {
     System.out.println("Introdueix un valor");
     String text = Entrada.readLine();
     valor = Integer.parseInt(text);
-    while (valor!=valorPensat || !text.isEmpty()) {
+    while (!text.isEmpty()) {
+    
+        if (valor!=valorPensat){
             
             if(valor>=101){
                 System.out.println("Com a màxim 100");
@@ -40,7 +42,7 @@ public static void main (String[] args) {
             System.out.println("Introdueix un valor");
             text = Entrada.readLine();
             valor = Integer.parseInt(text);
-    }
+    }}
     
         if  (valor==valorPensat ) { 
          
