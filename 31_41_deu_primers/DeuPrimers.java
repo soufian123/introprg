7119cbff97867613f3f58bccb669fb3520f135d7
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-10-25 10:01:32 +0200

    deuPrimers

---
+++ 31_41_deu_primers/DeuPrimers.java
@@ -0,0 +1,16 @@
+//un programa que escrigui els primers 10 números naturals. És a dir: 1 2 3…10
+
+
+public class DeuPrimers{
+    public static void main (String[] args) {
+    
+    
+        int numero = (0);
+        
+        while ( numero<=10){
+        numero = numero + 1;
+        System.out.println(numero);
+        }
+    }
+}
+        
