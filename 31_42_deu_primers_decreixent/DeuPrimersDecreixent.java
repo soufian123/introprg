602f835a463324fb40c4b8c12e679d7768dfd73d
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-10-25 10:04:54 +0200

    Els deu primers naturals decreixent

---
+++ 31_42_deu_primers_decreixent/DeuPrimersDecreixent.java
@@ -0,0 +1,17 @@
+// un programa que escrigui els primers 10 números naturals començant pel més gran. És a dir: 10 9 8 … 1
+
+
+public class DeuPrimersDecreixent{
+    public static void main (String[] args) {
+    
+    
+        int numero = (1);
+        
+        while ( numero<=10){
+                System.out.println(numero);
+                numero = numero - 1;
+
+        }
+    }
+}
+        
