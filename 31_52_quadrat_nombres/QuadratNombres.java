b95ade8236e652c75763f3a5886da5734cd6b7bd
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-11-03 20:22:07 +0100

    Un quadrat de nombres

---
+++ 31_52_quadrat_nombres/QuadratNombres.java
@@ -0,0 +1,24 @@
+
+//Escriu un programa que demani un enter positiu més gran que 0 i "dibuixi" un quadrat amb els nombres del 1 fins el valor de l'entrada.
+
+
+public class QuadratNombres{
+    public static void main (String[] args){
+    
+        
+        System.out.println("Valor final?");
+        int valor= Integer.parseInt(Entrada.readLine());
+        int inici=0;
+        if (valor>=1 && valor<=9){
+        
+            for (int lin = inici; lin<=valor; lin++){
+            
+                for (int col = inici; col<=valor; col++){
+                System.out.println(col);}
+            }        
+        
+        }
+        System.out.println("Valor inadequat");
+    }       
+}
+        
