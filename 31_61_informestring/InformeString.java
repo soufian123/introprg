6d53bd8312e0e0977c69bcfa519299c691a70e59
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-11-09 19:07:25 +0100

    aun no esta terminado bobo, terminalo vaaa

---
+++ 31_61_informestring/InformeString.java
@@ -0,0 +1,31 @@
+//Desenvolupa un programa que demani dos texts i un enter positiu i composi un petit informe amb el resultat de les funcions anteriors.
+
+
+public class InformeString{
+    public static void main (String[] args){
+    
+        
+        System.out.println("Text principal?");
+        String principal=Entrada.readLine();
+        System.out.println("Text secundari?");
+        String secundari=Entrada.readLine();
+        System.out.println("Número positiu?");
+        int numero= Integer.parseInt(Entrada.readLine());
+        
+        
+        
+        principal.length();
+        principal.startsWith(String unAltre);
+        principal.endsWith(String unAltre);
+        principal.equals(String unAltre);
+        principal.equalsIgnoreCase(String unAltre);
+        principal.isBlank();
+        principal.isEmpty();
+        principal.charAt(int posicio);
+        principal.concat(String unAltre);
+        principal.repeat(int cops);
+        principal.toUpperCase();
+        principal.toLowerCase();
+    
+    }
+}
