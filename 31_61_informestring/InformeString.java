b034938739769210daf4a1741d2de1d4b51dafea
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-11-10 20:18:02 +0100

    Informe d'un String

--- 31_61_informestring/InformeString.java
+++ 31_61_informestring/InformeString.java
@@ -12,20 +12,55 @@ public class InformeString{
         System.out.println("Número positiu?");
         int numero= Integer.parseInt(Entrada.readLine());
         
+        //Longitud de la cadena
+        int length= principal.length();
+        System.out.println(principal+".length(): "+ length);
+        
+        //Boolean que indica si la cadena comença per l'altre
+        boolean startsWith= principal.startsWith(secundari);
+        System.out.println(principal+".startsWith("+secundari+"): "+startsWith);
+        
+        //Boolean que indica si la cadena comença per l'altre
+        boolean endsWith= principal.endsWith(secundari);
+        System.out.println(principal+".endsWith("+secundari+"): " +endsWith);
+        
+        //Boolean que indica si la cadena és igual a l'altre
+        boolean equals= principal.equals(secundari);
+        System.out.println(principal+""+secundari+"): " +equals);
+        
+        //Boolean que indica si la cadena és igual a l'altre
+        boolean equalsIgnoreCase= principal.equalsIgnoreCase(secundari);
+        System.out.println(principal+".equalsIgnoreCase("+secundari+"): " +equalsIgnoreCase);
+        
+        //Boolean que indica si la cadena només conté caràcters blancs o bé està buida
+        boolean isBlank= principal.isBlank();
+        System.out.println(principal+".isBlank(): " +isBlank);
+        
+        //Boolean que indica si la cadena està buida
+        boolean isEmpty=principal.isEmpty();
+        System.out.println(principal+".isEmpty(): " +isEmpty);
+        
+        //Boolean que indica si la cadena està buida
+        char charAt= principal.charAt(numero);
+        System.out.println(principal+".charAt("+numero+"): "+charAt);
+        
+        //Cadena resultant de concatenar la cadena amb l'altre
+        String concat= principal.concat(secundari);
+        System.out.println(principal+".concat("+secundari+"): "+concat);
+        
+        //Cadena repetida el nombre de cops indicat
+        String repeat= principal.repeat(numero);
+        System.out.println(principal+".repeat("+numero+"): "+repeat);
+        
+        //Cadena amb tot en majúscules
+        String toUpperCase=principal.toUpperCase();
+        System.out.println(principal+".toUpperCase(): " +toUpperCase);
+        
+        //Cadena amb tot en minúscules
+        String toLowerCase=principal.toLowerCase();
+        System.out.println(principal+".toLowerCase(): "+toLowerCase);
         
         
-        principal.length();
-        principal.startsWith(String unAltre);
-        principal.endsWith(String unAltre);
-        principal.equals(String unAltre);
-        principal.equalsIgnoreCase(String unAltre);
-        principal.isBlank();
-        principal.isEmpty();
-        principal.charAt(int posicio);
-        principal.concat(String unAltre);
-        principal.repeat(int cops);
-        principal.toUpperCase();
-        principal.toLowerCase();
-    
+        
     }
 }
