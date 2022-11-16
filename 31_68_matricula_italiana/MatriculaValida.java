d5118919b9bf5c04cd15f11f6167ab2c560c97d7
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-11-16 23:55:27 +0100

    matricules  ∾ prgtest autocommit 31_68 #2

--- 31_68_matricula_italiana/MatriculaValida.java
+++ 31_68_matricula_italiana/MatriculaValida.java
@@ -8,7 +8,7 @@ public class MatriculaValida{
         String paraula= Entrada.readLine();
         
         if((paraula.length())==7){
-            if (Character.isLetter(paraula.charAt(0))==true && Character.isLetter(paraula.charAt(1))==true && Character.isLetter(paraula.charAt(2))==false && Character.isLetter(paraula.charAt(3))==false && Character.isLetter(paraula.charAt(4))==false && Character.isLetter(paraula.charAt(5))==true && Character.isLetter(paraula.charAt(6))==true){
+            if (Character.isLetter(paraula.charAt(0))==true && Character.isLetter(paraula.charAt(1))==true && Character.isLetter(paraula.charAt(2))==false && Character.isLetter(paraula.charAt(3))==false && Character.isLetter(paraula.charAt(4))==false && Character.isLetter(paraula.charAt(5))==true && Character.isLetter(paraula.charAt(6))==true && Character.isUpperCase(paraula.charAt(1))==true && Character.isUpperCase(paraula.charAt(0))==true && Character.isUpperCase(paraula.charAt(5))==true && Character.isUpperCase(paraula.charAt(6))==true){
                 System.out.println("És una matrícula italiana vàlida");
             }else{System.out.println("No és una matrícula italiana vàlida");
             }
