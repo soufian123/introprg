32283cbc8a3f140db244121be98764640a9bec49
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-11-16 22:25:38 +0100

    SegonaIgualPenultima  ∾ prgtest autocommit 31_67 #4

--- 31_67_segona_igual_penultima/SegonaIgualPenultima.java
+++ 31_67_segona_igual_penultima/SegonaIgualPenultima.java
@@ -14,7 +14,7 @@ public class SegonaIgualPenultima{
         String text= Entrada.readLine();
         
         if (!text.isBlank()){
-            while (!text.isBlank()){
+            while (!text.isBlank() || text.isEmpty()){
                 if ((text.length())>=2){
                         if (text.charAt(1) == text.charAt(text.length()-2)){
                              System.out.println("Segona igual a penúltima");
