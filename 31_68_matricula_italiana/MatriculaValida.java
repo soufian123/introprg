aa66bf1dbbd92ce8be333af99912f341629e7960
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-11-16 23:47:34 +0100

    matricules

---
+++ 31_68_matricula_italiana/MatriculaValida.java
@@ -0,0 +1,17 @@
+//programa per introduir nomes matricules italianes
+
+
+public class MatriculaValida{
+    public static void main (String[] args){
+        
+        System.out.println("Introduïu una matrícula");
+        String paraula= Entrada.readLine();
+        
+        
+        if (Character.isLetter(paraula.charAt(0))==true && Character.isLetter(paraula.charAt(1))==true && Character.isLetter(paraula.charAt(2))==false && Character.isLetter(paraula.charAt(3))==false && Character.isLetter(paraula.charAt(4))==false && Character.isLetter(paraula.charAt(5))==true && Character.isLetter(paraula.charAt(6))==true){
+            System.out.println("És una matrícula italiana vàlida");
+        }else{System.out.println("No és una matrícula italiana vàlida");
+        }
+    }
+}
+        
