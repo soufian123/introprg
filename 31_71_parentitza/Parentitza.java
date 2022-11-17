84f58257f92d70e877595898e82f0fe31b28cb3e
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-11-17 15:07:38 +0100

    lletres entre parentesis  ∾ prgtest autocommit 31_71 #2

--- 31_71_parentitza/Parentitza.java
+++ 31_71_parentitza/Parentitza.java
@@ -13,7 +13,7 @@ public class Parentitza{
         String text= Entrada.readLine();
         
         for(int num=0; num<text.length(); num++){
-            if (!(text.charAt(num)==' ')){
+            if (!(text.charAt(num)==' ' || Character.isLetter(text.charAt(num)))){
                 System.out.print("("+text.charAt(num)+")");
             }else{System.out.print(text.charAt(num));
             }
