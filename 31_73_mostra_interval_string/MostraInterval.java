44b8e13be73d52dcf2a2f9e17db8ebbe75defbfa
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-11-17 15:37:07 +0100

    mostra interval

---
+++ 31_73_mostra_interval_string/MostraInterval.java
@@ -0,0 +1,31 @@
+//imprimeix les lletres a base del inici y final que l'introdueixis
+
+
+
+
+
+
+
+public class MostraInterval{
+    public static void main (String[] args){
+    
+        System.out.println("Text?");
+        String text= Entrada.readLine();
+        System.out.println("inici?");
+        int inici= Integer.parseInt(Entrada.readLine());
+        System.out.println("final?");
+        int fin= Integer.parseInt(Entrada.readLine());
+        
+        
+        
+        for(int num=inici; num<fin; num++){
+            if (Character.isLetter(text.charAt(num))){
+                System.out.print(text.charAt(num));
+            }
+        }
+    }
+}
+        
+        
+        
+
