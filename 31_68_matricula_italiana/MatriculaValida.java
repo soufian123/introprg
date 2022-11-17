bac30b29cc0a8a78ad4e64b6a574162a51970120
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-11-17 10:37:15 +0100

    matricules  ∾ prgtest autocommit 31_68 #6

--- 31_68_matricula_italiana/MatriculaValida.java
+++ 31_68_matricula_italiana/MatriculaValida.java
@@ -6,13 +6,13 @@ public class MatriculaValida{
         
         System.out.println("Introduïu una matrícula");
         String paraula= Entrada.readLine();
-        String lletres= "IOQU";
+        String lletres= "IOQUÑ";
         String comprovador= "false";
         
         if((paraula.length())==7){
             if (Character.isLetter(paraula.charAt(0))==true && Character.isLetter(paraula.charAt(1))==true && Character.isLetter(paraula.charAt(2))==false && Character.isLetter(paraula.charAt(3))==false && Character.isLetter(paraula.charAt(4))==false && Character.isLetter(paraula.charAt(5))==true && Character.isLetter(paraula.charAt(6))==true && Character.isUpperCase(paraula.charAt(1))==true && Character.isUpperCase(paraula.charAt(0))==true && Character.isUpperCase(paraula.charAt(5))==true && Character.isUpperCase(paraula.charAt(6))==true && (paraula.charAt(2))>=0 ==true && (paraula.charAt(3))>=0 ==true && (paraula.charAt(4))>=0 ==true){
             
-            for (int num=0; num<=3; num++){
+            for (int num=0; num<=4; num++){
                 if (paraula.charAt(0)==lletres.charAt(num) || paraula.charAt(1) ==lletres.charAt(num) || paraula.charAt(5) ==lletres.charAt(num) || paraula.charAt(6) ==lletres.charAt(num)){
                     comprovador="true";
                 }
