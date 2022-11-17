efd07b4118314c9cc59d85de5fc410546e057baa
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-11-17 20:16:27 +0100

    es enter

---
+++ 31_75_es_enter/EsEnter.java
@@ -0,0 +1,33 @@
+//Desenvolupa un programa que vagi demanant texts fins rebre la cadena buida.
+
+//Per cada text no buit que rebi, indicarà si correspon o no a un nombre enter escrit amb dígits.
+
+
+public class EsEnter{
+    public static void main (String[] args){
+    
+        System.out.println("Text?");
+        String text= Entrada.readLine();
+        String numeros= ("0123456789+-");
+        String comprovar="false";
+        while (!(text.isBlank())){
+            for(int num=0; num<=(text.length()+1); num++){
+                
+                for(int n=0; num<=(numeros.length()+1); num++){
+                    if(!(text.charAt(num)== numeros.charAt(n))){
+                        comprovar="true";
+                    }
+                }
+            }
+            if (comprovar=="true"){
+                System.out.println("No és enter");
+            
+            }else{System.out.println("És enter");}
+            
+            text= Entrada.readLine();
+            comprovar="false";
+        }
+        System.out.println("Adéu");
+        
+    }
+}                
