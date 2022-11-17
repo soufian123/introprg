5c64ba78c395d1dde57275d7bbdb6f47040d532a
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-11-17 15:37:28 +0100

    mostra interval

--- 31_73_mostra_interval_string/MostraInterval.java
+++ 31_73_mostra_interval_string/MostraInterval.java
@@ -9,7 +9,7 @@
 public class MostraInterval{
     public static void main (String[] args){
     
-        System.out.println("Text?");
+        System.out.println("text?");
         String text= Entrada.readLine();
         System.out.println("inici?");
         int inici= Integer.parseInt(Entrada.readLine());
