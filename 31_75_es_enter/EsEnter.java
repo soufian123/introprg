732df8c4df018784391653fdf47a3c51a9c95fec
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-11-17 20:21:42 +0100

    es enter  ∾ prgtest autocommit 31_75 #2

--- 31_75_es_enter/EsEnter.java
+++ 31_75_es_enter/EsEnter.java
@@ -11,22 +11,24 @@ public class EsEnter{
         String numeros= ("0123456789+-");
         String comprovar="false";
         while (!(text.isBlank())){
-            for(int num=0; num<=(text.length()+1); num++){
-                
-                for(int n=0; n<=(numeros.length()+1); n++){
-                    if(!(text.charAt(num) == numeros.charAt(n))){
-                        comprovar="true";
+        
+            if (!(text.isBlank())){
+                for(int num=0; num<=(text.length()+1); num++){
+                    
+                    for(int n=0; n<=(numeros.length()+1); n++){
+                        if(!(text.charAt(num) == numeros.charAt(n))){
+                            comprovar="true";
+                        }
                     }
                 }
+                if (comprovar=="true"){
+                    System.out.println("No és enter");
+                
+                }else{System.out.println("És enter");}
+            }    
+                text= Entrada.readLine();
+                comprovar="false";
             }
-            if (comprovar=="true"){
-                System.out.println("No és enter");
-            
-            }else{System.out.println("És enter");}
-            
-            text= Entrada.readLine();
-            comprovar="false";
-        }
         System.out.println("Adéu");
         
     }
