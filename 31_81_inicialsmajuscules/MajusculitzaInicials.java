ee28b3911f9a1f956322235848b34e8358e7804c
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-11-20 17:27:12 +0100

    majuscules al inici

---
+++ 31_81_inicialsmajuscules/MajusculitzaInicials.java
@@ -0,0 +1,36 @@
+//posa les majuscules a les inicials
+
+
+
+public class MajusculitzaInicials{
+    public static void main (String[] args){
+        
+        
+        System.out.println("Text?");
+        String text= Entrada.readLine();
+        
+        
+        String seguenMajuscula="false";
+        
+        System.out.print(Character.toUpperCase(text.charAt(0)));
+        text= text.toLowerCase();
+        
+        for(int num=1; num<text.length(); num++){
+        
+            if(seguenMajuscula=="true"){
+                System.out.print(Character.toUpperCase(text.charAt(num)));
+                seguenMajuscula="false";
+            }else if(seguenMajuscula=="false"){
+                if (Character.isLetter(text.charAt(num))==false){
+                    seguenMajuscula="true";
+                    System.out.print(text.charAt(num));
+                    
+                }else{
+                    System.out.print(text.charAt(num));
+                }
+            }
+
+        }
+        
+    }
+}
