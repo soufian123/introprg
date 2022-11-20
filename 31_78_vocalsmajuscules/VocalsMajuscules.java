e12d0ff07e67a164352bc8a946c6a4f182f9e6f6
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-11-20 16:44:03 +0100

    Vocals en majúscules

---
+++ 31_78_vocalsmajuscules/VocalsMajuscules.java
@@ -0,0 +1,23 @@
+//Desenvolupa un programa que demani un text i mostri totes les lletres en minúscules excepte les vocals, que hauran d'estar en majúscules.
+
+
+
+public class VocalsMajuscules{
+    public static void main (String[] args){
+        
+        String vocals= "aeiou"; 
+        
+        System.out.println("Text?");
+        String text= Entrada.readLine();
+        
+        for(int num=0; num<text.length(); num++){
+            for (int v=0; v<vocals.length(); v++){
+                if(text.charAt(num)==vocals.charAt(v)){
+                    System.out.print(Character.toUpperCase(text.charAt(num)));
+                }else{
+                    System.out.print(text.charAt(num));
+                }
+            }
+        }
+    }
+}
