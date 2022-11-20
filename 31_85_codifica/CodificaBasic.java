8863d4f3ba4dfcf6a22a8376bc812e0ed36031a8
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-11-20 22:38:20 +0100

    codificar text  ∾ prgtest autocommit 31_85 #27

--- 31_85_codifica/CodificaBasic.java
+++ 31_85_codifica/CodificaBasic.java
@@ -12,13 +12,13 @@ public class CodificaBasic{
         
         System.out.println("Text?");
         String text= Entrada.readLine();
-        String lletres="qwertyuiopasdfghjklñxcvbnm";
+        String vocals="áéíúóàèìùòäëïöü";
         char ch= text.charAt(0);
-        
+        String accents= "false";
 
         for(int num=0; num<text.length(); num++){
             ch= text.charAt(num);
-            
+            accents= "false";
                 if(text.charAt(num)=='@'){
                     System.out.print("@");
                 }else if (!(Character.isLetter(text.charAt(num)))){
@@ -27,9 +27,28 @@ public class CodificaBasic{
                     System.out.print("a");
                 }else if(text.charAt(num)==' '){
                     System.out.print(" ");
+                
                     
                     
                 }else if(Character.isLetter(text.charAt(num)) && Character.isLowerCase(text.charAt(num))){
+                    
+                for (int v=0; v<vocals.length(); v++){
+            
+                    if(text.charAt(num)==vocals.charAt(v)){
+                        System.out.print(text.charAt(num));
+                        accents="true";
+                        break;
+                    }
+            }
+            if(accents=="true"){
+                System.out.print((char)(ch));
+            }else{
+                System.out.print((char)(ch+1));
+            }
+                    
+                    
+                    
+                    
                     System.out.print((char)(ch+1));
                     }else{ 
                         System.out.print((char)(ch));
