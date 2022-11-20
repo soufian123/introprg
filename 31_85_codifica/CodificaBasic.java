ff8c3c40f3b29aef1fb706d16ab08c8fa6a8be24
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-11-20 22:04:32 +0100

    codificar text  ∾ prgtest autocommit 31_85 #8

--- 31_85_codifica/CodificaBasic.java
+++ 31_85_codifica/CodificaBasic.java
@@ -23,7 +23,7 @@ public class CodificaBasic{
                 }
                 if(text.charAt(num)=='z'){
                     System.out.print("a");
-                }else if (Character.isLetter(text.charAt(num))){
+                }else{
                     ch= text.charAt(num);
                     System.out.print((char)(ch+1));
                 }    
