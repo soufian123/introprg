3ec0561d22e3ed35b30c5430bfe518e1db9a7c76
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-11-20 20:26:01 +0100

    texts capicua  ∾ prgtest autocommit 31_84 #2

--- 31_84_textcapicua/Capicua.java
+++ 31_84_textcapicua/Capicua.java
@@ -15,27 +15,32 @@ public class Capicua{
         String verificacio="false";
         
         if (!text.isBlank()){
-            if (numero==0 || numero % 2 == 0){
+            if (numero==0){
                 System.out.println("No és capicua");
             }else{
-                numero= (text.length()-1)/2;
-                int resta= numero;
-                for (int num=0;num<=numero; num++){
-                    if(text.charAt(num)==text.charAt(resta)){
-                        verificacio="true";
-                        resta=resta-1;
-                    }else{ 
-                        verificacio="false";
-                        break;
-                    }
-                }
-                if (verificacio=="false"){
+            
+                if (numero % 2 == 0){
                     System.out.println("No és capicua");
+                }else{
+                    numero= (text.length()-1)/2;
+                    int resta= numero;
+                    for (int num=0;num<=numero; num++){
+                        if(text.charAt(num)==text.charAt(resta)){
+                            verificacio="true";
+                            resta=resta-1;
+                        }else{ 
+                            verificacio="false";
+                            break;
+                        }
+                    }
+                    if (verificacio=="false"){
+                        System.out.println("No és capicua");
+                    }
+                    if (verificacio=="true"){
+                        System.out.println("És capicua");
+                    }
+                    verificacio="false";
                 }
-                if (verificacio=="true"){
-                    System.out.println("És capicua");
-                }
-                verificacio="false";
             }
         }
     }
