8dfd228c6526c373dede94ea2a0370c553d7778b
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-11-22 11:05:40 +0100

    Paraules del revés  ∾ prgtest autocommit 31_91 #28

--- 31_91_paraulesreves/ParaulesReves.java
+++ 31_91_paraulesreves/ParaulesReves.java
@@ -6,17 +6,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 public class ParaulesReves{
     public static void main(String[] args){
         
@@ -26,6 +15,8 @@ public class ParaulesReves{
         
         int espai=0;
         String verificar="false";
+        String lletra="false";
+        String nombres="false";
         
         for(int num=0; num<text.length(); num++){
             
@@ -34,8 +25,28 @@ public class ParaulesReves{
                 break;
             }
         }
+        for(int num=0; num<text.length(); num++){
+            if(Character.isLetter(text.charAt(num))){
+                lletra="true";
+                
+            }else{
+                lletra="false";
+                break;
+            }
+        }
         
-        if(verificar=="true"){
+        for(int num=0; num<text.length(); num++){
+            if(Character.isDigit(text.charAt(num))){
+                nombres="true";
+            }else{
+                nombres="false";
+                break;
+            
+            }
+        
+        }
+        
+        if(verificar=="true" && ( lletra=="true" || nombres=="true")){
         
         
             for(int num=0; num<text.length(); num++){
@@ -52,14 +63,47 @@ public class ParaulesReves{
                 }
                 
             }
+            
             for (int numero=text.length()-1; numero>espai-1; numero--){
                 System.out.print(text.charAt(numero));
             }
 
-        }else{
+        }else if (verificar=="false" && ( lletra=="true" || nombres=="true") ){
             for (int numero=text.length()-1; numero>=0; numero--){
                 System.out.print(text.charAt(numero));
             }
+            
+            
+            
+            
+            
+        }else if(lletra=="false"){
+        
+        
+            for(int num=0; num<text.length(); num++){
+                
+                if(Character.isWhitespace(text.charAt(num)) && Character.isLetter(text.charAt(num))){
+                    
+                    for (int numero=num-1; numero>=espai; numero--){
+                        System.out.print(text.charAt(numero));
+                    }
+                    System.out.print(" ");
+                    
+                    espai=num+1;
+                    
+                }else{
+                    System.out.print(text.charAt(num));
+                }
+            
+                if(Character.isWhitespace(text.charAt(num)) && Character.isLetter(text.charAt(num))){
+                    for (int numero=text.length()-1; numero>espai-1; numero--){
+                    System.out.print(text.charAt(numero));
+                    }
+                }else{
+                    System.out.print(text.charAt(num));
+                }
+            }
+
         }
         
     }
