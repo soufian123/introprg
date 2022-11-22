0df4eeaa953425e2232a8604ff905caf54fa11ff
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-11-22 10:39:46 +0100

    Paraules del revés  ∾ prgtest autocommit 31_91 #23

--- 31_91_paraulesreves/ParaulesReves.java
+++ 31_91_paraulesreves/ParaulesReves.java
@@ -47,7 +47,7 @@ public class ParaulesReves{
                     }
                     System.out.print("");
                     
-                    espai=num-1;
+                    espai=num+1;
                     
                 }
             }
