7371254462fa302708682cd92a29360df499ee56
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-11-28 10:36:03 +0100

    Quadrats modulars

---
+++ 32_03_quadrats_modulars/Quadrats.java
@@ -0,0 +1,47 @@
+/*
+
+
+un programa que demani un número per entrada estàndard, i dibuixi tants quadrats com indiqui el número. Si el número és 0 o negatiu, no dibuixarà res, pero des de dibuixaQuadrats() es cridarà un mòdul anomenat dibuixaQuadrat() que farà la feina de dibuixar les difents línies del quadrat fent servir dibuixaLinia()
+
+
+
+*/
+
+
+
+public class Quadrats{
+    public static void main (String[] args){
+        
+        dibuixaQuadrats();
+    }
+        
+    public static void dibuixaQuadrats(){
+        //preguntar quants
+        System.out.println("Quants?");
+        int quants=Integer.parseInt(Entrada.readLine());
+        if(quants>0){
+        //repeteix quadrat
+            for (int num=1;num<=quants; num++){
+                //imprimeix lineas
+                dibuixaLinia();
+            }
+                //dibuixa salt
+                System.out.println();
+                
+        }
+    }
+    
+    
+    
+    
+    
+    
+    public static void dibuixaLinia(){
+            //dibuixa en columnes
+            for(int col=1; col<6;col++){
+                //dibuixa X
+                System.out.print(" X");
+            }
+    }
+
+}               
