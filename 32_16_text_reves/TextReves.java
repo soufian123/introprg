a42f74e9a202c91dc7e8bf87bce14e0f298e1350
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-11-30 16:56:24 +0100

    El text del revés (versió bàsica)

---
+++ 32_16_text_reves/TextReves.java
@@ -0,0 +1,40 @@
+//Desenvolupa un programa que demani un text i el torni a mostrar però invertint l'ordre dels caràcters que el composen. Els caracters resultants apareixeran separats per comes. (moduls)
+
+
+
+
+
+
+
+public class TextReves{
+    public static void main (String[] args){
+    
+        System.out.println("Text?");
+        String text= Entrada.readLine();
+        mostraReves(text);
+
+    }
+    
+    
+    public static void mostraReves(String text){
+    
+        if (!text.isBlank()){
+            int fin=0;
+            for(int num=text.length()-1; num>=0; num--){
+
+                    fin=num;
+                    num=0;
+                
+            }
+            
+            
+            for(int num=fin; num>0; num--){
+
+                    System.out.print(text.charAt(num)+", ");
+              }  
+            System.out.print(text.charAt(0));
+        }
+    
+    
+    }
+}
