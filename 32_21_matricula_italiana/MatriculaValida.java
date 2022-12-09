2c1d98f5a7d0a96982e8f7b6698a3c88f4c3de51
prgtest <moises.gomez@iesjoandaustria.org>
2022-12-09 23:40:47 +0100

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 32_21 #3

--- 32_21_matricula_italiana/MatriculaValida.java
+++ 32_21_matricula_italiana/MatriculaValida.java
@@ -5,20 +5,18 @@ public class MatriculaValida{
     public static void main (String[] args){
         
         System.out.println("Introduïu una matrícula");
-        String lletra= Entrada.readLine();
+        String paraula= Entrada.readLine();
         String lletres= "IOQUÑ";
         String comprovador= "false";
-        char res='r';
-        boolean testDos=false;
         
         
-        if((lletra.length())==7){
-        //boolean test = esLletraValidaPerMatriculaItaliana(lletra ,testDos);
-            boolean obtingut = MatriculaValida.esLletraValidaPerMatriculaItaliana(lletra);
+        if((paraula.length())==7){
+        //boolean test = esLletraValidaPerMatriculaItaliana(paraula ,testDos);
+            boolean obtingut = MatriculaValida.esLletraValidaPerMatriculaItaliana(paraula);
             if (obtingut==true){
             
             for (int num=0; num<=4; num++){
-                if (lletra.charAt(0)==lletres.charAt(num) || lletra.charAt(1) ==lletres.charAt(num) || lletra.charAt(5) ==lletres.charAt(num) || lletra.charAt(6) ==lletres.charAt(num)){
+                if (paraula.charAt(0)==lletres.charAt(num) || paraula.charAt(1) ==lletres.charAt(num) || paraula.charAt(5) ==lletres.charAt(num) || paraula.charAt(6) ==lletres.charAt(num)){
                     comprovador="true";
                 }
             }
@@ -36,9 +34,9 @@ public class MatriculaValida{
     
     
     
-    public static boolean esLletraValidaPerMatriculaItaliana(String lletra){
+    public static boolean esLletraValidaPerMatriculaItaliana(String paraula){
         
-        if (Character.isLetter(lletra.charAt(0))==true && Character.isLetter(lletra.charAt(1))==true && Character.isLetter(lletra.charAt(2))==false && Character.isLetter(lletra.charAt(3))==false && Character.isLetter(lletra.charAt(4))==false && Character.isLetter(lletra.charAt(5))==true && Character.isLetter(lletra.charAt(6))==true && Character.isUpperCase(lletra.charAt(1))==true && Character.isUpperCase(lletra.charAt(0))==true && Character.isUpperCase(lletra.charAt(5))==true && Character.isUpperCase(lletra.charAt(6))==true && (lletra.charAt(2))>=0 ==true && (lletra.charAt(3))>=0 ==true && (lletra.charAt(4))>=0 ==true){
+        if (Character.isLetter(paraula.charAt(0))==true && Character.isLetter(paraula.charAt(1))==true && Character.isLetter(paraula.charAt(2))==false && Character.isLetter(paraula.charAt(3))==false && Character.isLetter(paraula.charAt(4))==false && Character.isLetter(paraula.charAt(5))==true && Character.isLetter(paraula.charAt(6))==true && Character.isUpperCase(paraula.charAt(1))==true && Character.isUpperCase(paraula.charAt(0))==true && Character.isUpperCase(paraula.charAt(5))==true && Character.isUpperCase(paraula.charAt(6))==true && (paraula.charAt(2))>=0 ==true && (paraula.charAt(3))>=0 ==true && (paraula.charAt(4))>=0 ==true){
             return true;
         }else{ 
             return false;
