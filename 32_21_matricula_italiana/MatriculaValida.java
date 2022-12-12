481a1baa66c7f4bbfbe8af4a6d7dd3de822bcb27
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-12-12 16:58:27 +0100

    Amb Xavi  ∾ prgtest autocommit 32_21 #0

--- 32_21_matricula_italiana/MatriculaValida.java
+++ 32_21_matricula_italiana/MatriculaValida.java
@@ -8,11 +8,12 @@ public class MatriculaValida{
         String paraula= Entrada.readLine();
         String lletres= "IOQUÑ";
         String comprovador= "false";
-        boolean obtingut = MatriculaValida.esLletraValidaPerMatriculaItaliana(paraula);
+
         
         if((paraula.length())==7){
         //boolean test = esLletraValidaPerMatriculaItaliana(paraula ,testDos);
             boolean obtingut = MatriculaValida.esLletraValidaPerMatriculaItaliana(paraula);
+            
             if (obtingut==true){
             
             for (int num=0; num<=4; num++){
