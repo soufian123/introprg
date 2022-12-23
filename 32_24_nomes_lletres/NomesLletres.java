7de2341d3e8f02192c5c9056386ec23dd0458451
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-12-24 00:00:29 +0100

    Només lletres (amb mòduls)

---
+++ 32_24_nomes_lletres/NomesLletres.java
@@ -0,0 +1,15 @@
+/*
+ * Desenvolupa un programa que demani un text i mostri només les lletres que conté. 
+ * Cada lletra apareixerà separada per una coma en l'ordre en que apareixia al text original. 
+ * La resta de caràcters no es mostraran.
+ */
+
+public class NomesLletres{
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        String nomesLletres = UtilString.nomesLletres(text);
+        String separades = UtilString.lletresSeparades(nomesLletres);
+        System.out.println(separades);
+    }
+}
---
+++ 32_24_nomes_lletres/UtilString.java
@@ -0,0 +1,58 @@
+
+//utilitats del String      UtilString
+
+
+
+
+
+
+
+
+public class UtilString{
+
+    public static String nomesLletres(String text){
+    
+        String lletres="";
+        int fin=0;
+        for(int num=text.length()-1; num>=0; num--){
+            if (Character.isLetter(text.charAt(num))){
+                fin=num;
+                num=0;
+            }
+        }
+        for(int num=0; num<=fin; num++){
+            if (Character.isLetter(text.charAt(num))){
+                lletres = lletres + text.charAt(num);
+            }
+        }
+        return lletres;
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static String lletresSeparades(String nomesLletres){
+        String separat=""; 
+        for(int num=0; num<nomesLletres.length(); num++){
+            if (Character.isLetter(nomesLletres.charAt(num))){
+                separat = (separat + nomesLletres.charAt(num)+", ");
+            }
+            
+        }
+        separat = separat + nomesLletres.charAt(nomesLletres.length()-1);
+    return separat;
+    
+    }
+
+
+
+}
+
+
