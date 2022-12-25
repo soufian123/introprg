6ff46a35131c2e30fbeca283481dc0ef2c8b21c9
prgtest <moises.gomez@iesjoandaustria.org>
2022-12-25 01:57:19 +0100

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 32_24 #8

--- 32_24_nomes_lletres/UtilString.java
+++ 32_24_nomes_lletres/UtilString.java
@@ -13,7 +13,10 @@ public class UtilString{
     public static String nomesLletres(String text){
         String lletres="";
         int fin=0;
-
+        if (lletres==","){
+            lletres="";
+            return lletres;
+        }
         for(int num=text.length()-1; num>=0; num--){
             if (Character.isLetter(text.charAt(num))){
                 fin=num;
