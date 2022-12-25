3da607f4467e78fa8dbfdd5f02c9caedbecc9c81
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-12-25 02:49:08 +0100

    És enter (modular)  ∾ prgtest autocommit 32_27 #5

--- 32_27_es_enter/UtilString.java
+++ 32_27_es_enter/UtilString.java
@@ -10,61 +10,30 @@
 public class UtilString{
 
     public static boolean esEnter(String text){
-        String numeros= ("0123456789+-");
-    
+
+
+        boolean resultat=false;
     
         for(int num=0; num<=(text.length()-1); num++){
             if(!(Character.isDigit(text.charAt(num)) || Character.isWhitespace(text.charAt(num))|| text.charAt(num)=='+' || text.charAt(num)=='-')){
-                return true;
+                resultat=true;
             }
+            
             if((text.length()-2)<=num && text.charAt(num)=='+'){
-                return true;
+                resultat=true;
             }
                     
             if((text.length()-2)<=num && text.charAt(num)=='-'){
-                return true;
+                resultat=true;
             }
         }
-        return true;
+        return resultat;
     }
 }
 
 
 
 
-/*
 
-public class EsEnter{
-    public static void main (String[] args){
-    
-        System.out.println("Introdueix texts (enter sol per finalitzar)");
-        String text= Entrada.readLine();
-        String numeros= ("0123456789+-");
-        String comprovar="false";
-        while (!(text.isBlank() || text.isEmpty())){
-        
-            if (!(text.isBlank() || text.isEmpty())){
-                for(int num=0; num<=(text.length()-1); num++){
-                    if(!(Character.isDigit(text.charAt(num)) || Character.isWhitespace(text.charAt(num))|| text.charAt(num)=='+' || text.charAt(num)=='-')){
-                        comprovar="true";
-                    }
-                    if((text.length()-2)<=num && text.charAt(num)=='+'){
-                        comprovar="true";
-                    }
-                    
-                    if((text.length()-2)<=num && text.charAt(num)=='-'){
-                        comprovar="true";
-                    }
-                }
-                if (comprovar=="true"){
-                    System.out.println("No és enter");
-                
-                }else{System.out.println("És enter");}
-            }    
-                text= Entrada.readLine();
-                comprovar="false";
-            }
-        System.out.println("Adéu");
-        
-    }
-}   */
+
+
