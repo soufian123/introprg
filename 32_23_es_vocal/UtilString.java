93fe63e33bc9ca00b487b241f32fea69b296e4ff
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2022-12-25 02:01:01 +0100

    tot correcte

--- 32_23_es_vocal/UtilString.java
+++ 32_23_es_vocal/UtilString.java
@@ -1,4 +1,4 @@
-//et diu si es true o fals les vocals
+//et diu si es true o fals les vocals, es a dir, revisa el char(la lletra) que li entra si coincideix en la lletra en una de les vocals que hi ha asignat en el String vocal, retorna un true, en el cas de que no ho sigui retorna un false
 
 public class UtilString{
     public static boolean esVocal(char inicial){
