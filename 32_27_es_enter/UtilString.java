d00605b549cb60c763957baab1d585a966a7d830
prgtest <cf22soufian.el@iesjoandaustria.org>
2022-12-25 03:03:27 +0100

    És enter (modular)  ∾ prgtest autocommit 32_27 #12

--- 32_27_es_enter/UtilString.java
+++ 32_27_es_enter/UtilString.java
@@ -1,7 +1,7 @@
 
 
 
-
+//retorna un balor boolean cuan comprova si es enter o no
 
 
 
@@ -15,16 +15,9 @@ public class UtilString{
         boolean resultat=true;
     
         for(int num=0; num<=(text.length()-1); num++){
-            if (text=="-1" || text=="+1"){
-            return true;
-            }
-            if (text=="" || text=="  " || text==" 1" || text=="1 "){
-            return false;
-            }
-            
-            
+      
             if(!(Character.isDigit(text.charAt(num)) || Character.isWhitespace(text.charAt(num))|| text.charAt(num)=='+' || text.charAt(num)=='-')){
-                resultat=true;
+                resultat=false;
             }
             
             if((text.length()-2)<=num && text.charAt(num)=='+'){
