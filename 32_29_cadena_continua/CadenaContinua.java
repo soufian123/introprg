94f6274b0b00f646a069a7cb96c9860e369cc04d
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-01-08 15:16:42 +0100

    29 cadena continua

--- 32_29_cadena_continua/CadenaContinua.java
+++ 32_29_cadena_continua/CadenaContinua.java
@@ -1,1 +1,26 @@
-b
+//Desenvolupa un programa que demani un text i un nombre enter, i mostri tants caràcters del text com indiqui el nombre enters, començant pel primer. En cas que en faltin, el programa tornarà a mostrar el text a partir del primer caràcter fins que hagi aconseguit tots els caràcters demanats. (modular)
+
+
+public class CadenaContinua{
+    public static void main (String[] args){
+    
+        System.out.println("Text?");
+        String text= Entrada.readLine();
+
+        if (!(text.isBlank())){
+        
+        System.out.println("Nombre?");
+            String nombre= Entrada.readLine();
+            
+            if(UtilString.esEnter(nombre)){
+            
+                int numero = Integer.parseInt(nombre);
+			    String sortida = UtilString.cadenaContinua(text, numero);
+			    System.out.println(sortida);
+            
+            }else{System.out.println("error");}
+            
+        }else{System.out.println("error");
+        }
+    }
+} 
--- 32_29_cadena_continua/UtilString.java
+++ 32_29_cadena_continua/UtilString.java
@@ -1,1 +1,63 @@
-b
+
+
+
+//retorna un valor boolean cuan comprova si es enter o no
+
+
+
+
+
+public class UtilString{
+
+    public static boolean esEnter(String text /*, boolean estricte */){
+
+
+        //comprova si tot es lletra o numero 
+        for(int num=0; num<=(text.length()-1); num++){
+            if(Character.isLetter(text.charAt(num))){
+                return false;
+            }else {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+
+
+
+
+
+
+
+
+
+    public static String cadenaContinua(String text, int cops){
+        
+        
+        String sortida="";
+        if (cops>=0){
+            int numero=0;
+            for(int num=0; num<cops; num++){
+                
+                if (numero<text.length()){
+                }else{
+                    numero=0;
+                }
+                
+                sortida= sortida + text.charAt(numero);
+                numero= numero+1;
+            }
+        }
+        return sortida;
+
+    }
+
+
+}
+
+
+
+
+
+
