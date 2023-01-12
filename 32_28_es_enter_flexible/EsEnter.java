da67855efff0c4d4bf3b8b32598fc9788f3364c3
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-01-12 19:20:00 +0100

    Transformació de formes  ∾ prgtest autocommit 32_28 #1

--- 32_28_es_enter_flexible/EsEnter.java
+++ 32_28_es_enter_flexible/EsEnter.java
@@ -23,8 +23,8 @@ public class EsEnter{
                 texto= texto + text.charAt(num);
             }
         }
-                obtingut = UtilString.esEnter(text /*, obtingut */);
-                numero= UtilString.aEnter(text , obtingut );
+                obtingut = esEnter(text /*, obtingut */);
+                numero= aEnter(text , obtingut );
                 
                 if (numero==1){
                     System.out.println("És enter");
@@ -37,6 +37,75 @@ public class EsEnter{
         System.out.println("Adéu");
         
     }
+        public static boolean esEnter(String text /*, boolean estricte */){
+
+
+        boolean resultat=true;
+        boolean texts=true;
+        String texto="";
+        //treu tots els espais en blanc, + i -
+        for(int num=0; num<=(text.length()-1); num++){
+            if( Character.isWhitespace(text.charAt(num))|| text.charAt(num)=='+' || text.charAt(num)=='-'){
+            }else {
+                texto= texto + text.charAt(num);
+            }
+        }
+        //comprova si tot es lletra o numero 
+        for(int num=0; num<=(texto.length()-1); num++){
+            if(Character.isLetter(texto.charAt(num))){
+
+            }else {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+
+
+    public static int aEnter(String text , boolean texts ){
+
+        boolean resultat=true;
+        String texto="";
+        //treu tots els espais en blanc, + i -
+        for(int num=0; num<=(text.length()-1); num++){
+            if( Character.isWhitespace(text.charAt(num))|| text.charAt(num)=='+' || text.charAt(num)=='-'){
+            }else {
+                texto= texto + text.charAt(num);
+            }
+        }
+        //comprovacio si es enter o no en el cas de lletra
+        if (texts){
+            int numeroLletra=(text.length()-1);
+            if (numeroLletra % 2 == 0){
+                resultat= true;
+            }else{
+                resultat=false;
+            }
+
+        }
+
+        //comprovacio si es enter o no en el cas de numero
+        if(texts==false){
+
+            int numero= Integer.parseInt((texto));
+            if (numero % 2 == 0){
+                resultat= false;
+            }else{
+                resultat=true;
+            }
+
+        }
+        if (resultat){
+            return 1;
+        }else{
+            return 2;
+        }
+
+    
+    
+    }
+    
 } 
 
 
