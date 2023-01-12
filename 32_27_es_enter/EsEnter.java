e1e29e0455cb3392b5dbb1c41314105a92e4fbb5
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-01-12 20:21:52 +0100

    1  ∾ prgtest autocommit 32_27 #13

--- 32_27_es_enter/EsEnter.java
+++ 32_27_es_enter/EsEnter.java
@@ -9,7 +9,7 @@ public class EsEnter{
         System.out.println("Introdueix texts (enter sol per finalitzar)");
         String text= Entrada.readLine();
         boolean comprovar= false;
-
+        text = text.strip();
         while (!(text.isBlank() || text.isEmpty())){
         
         
