79fc6e7d9cee525633c4242995dc31e8014885db
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-01-12 20:29:01 +0100

    1

--- 32_29_cadena_continua/CadenaContinua.java
+++ 32_29_cadena_continua/CadenaContinua.java
@@ -1,5 +1,5 @@
 //Desenvolupa un programa que demani un text i un nombre enter, i mostri tants caràcters del text com indiqui el nombre enters, començant pel primer. En cas que en faltin, el programa tornarà a mostrar el text a partir del primer caràcter fins que hagi aconseguit tots els caràcters demanats. (modular)
-
+ 
 
 public class CadenaContinua{
     public static void main (String[] args){
