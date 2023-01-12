8b0a39b3a1026986acfba3cf32c96adf2a5e06e2
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-01-12 20:07:50 +0100

    1  ∾ prgtest autocommit 32_27 #4

--- 32_27_es_enter/UtilString.java
+++ 32_27_es_enter/UtilString.java
@@ -8,23 +8,25 @@
 
 
 public class UtilString{
-//retorna un valor boolean cuan comprova si es enter o no
-    public static boolean esEnter(String text){
-
-
-        boolean resultat=true;
-        boolean texts=true;
-        
+//retorna un boolean cuan comprova si es enter o no
         //comprova si tot es lletra o numero 
-        for(int num=0; num<=(text.length()-1); num++){
-            if(Character.isLetter(text.charAt(num))){
+    public static boolean esEnter(String text) {
+        if (text.isBlank()) {
+            return false;
+        }
+        
+        for (int i=0; i<text.length();i++) {
+            if (Character.isLetter(text.charAt(i))) {
+                return false;
+                
+            } else if (text.charAt(i) == ',' || text.charAt(i) == ' ') {
                 return false;
             }
+            
         }
         return true;
     }
 }
-
 
 
 
