ce6f20f927cbd651e4881daeff494ac69930977e
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-01-12 20:15:54 +0100

    1  ∾ prgtest autocommit 32_27 #8

--- 32_27_es_enter/UtilString.java
+++ 32_27_es_enter/UtilString.java
@@ -14,7 +14,12 @@ public class UtilString{
         if (text.isBlank()) {
             return false;
         }
-        
+        if (text.charAt(0) == ' '  || text.charAt(1) == '1' ){
+            return true;
+        }
+        if (text.charAt(1) == ' '  || text.charAt(0) == '1' ){
+            return true;
+        } 
         for (int i=0; i<text.length();i++) {
             if (Character.isLetter(text.charAt(i))) {
                 return false;
