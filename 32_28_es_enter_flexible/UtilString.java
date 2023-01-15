b38d52be16beabebdf1dedffc08a9108890508ce
prgtest <moises.gomez@iesjoandaustria.org>
2023-01-15 15:50:11 +0100

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 32_28 #23

--- 32_28_es_enter_flexible/UtilString.java
+++ 32_28_es_enter_flexible/UtilString.java
@@ -40,6 +40,9 @@ public class UtilString{
                 texto= texto + text.charAt(num);
             }
         }
+        if (texto.length()==0){
+            return 0;
+        }
         //comprovacio si es enter o no en el cas de lletra
         if (texts){
             int pasar= Integer.parseInt(text);
