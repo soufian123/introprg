419f9316fe7a7e39546db14bc6381aa4d565168d
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-01-16 20:09:54 +0100

    Inicialització seqüencial  ∾ prgtest autocommit 32_47 #10

--- 32_47_inisequencial/UtilTaula.java
+++ 32_47_inisequencial/UtilTaula.java
@@ -33,7 +33,6 @@ el programa substitueix() espera una taula de N x M caràcters, i dos caràcters
         for(int l=0; l<taula.length; l++){
             for(int c=0; c<taula[0].length; c++){
                 taula[l][c]= valor;
-                valor++;
             }
         }
         
