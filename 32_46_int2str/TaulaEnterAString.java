0929888ef679ce7d35a44eaab62c67e410316337
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-01-16 17:31:49 +0100

    1

---
+++ 32_46_int2str/TaulaEnterAString.java
@@ -0,0 +1,128 @@
+/*
+un programa que permet mostrar taules de diferents dimensions, amb totes les posicions inicialitzades amb un 1.
+*/
+
+public class TaulaEnterAString {
+
+    public static void main(String[] args){
+
+        for (int i=0; i<args.length; i++) {
+
+            System.out.println(args[i]);
+
+            int files = obteFiles(args[i]);
+
+            int columnes = obteColumnes(args[i]);
+
+            if (files < 1 || columnes < 1) {
+
+                System.out.println("Especificació no vàlida");
+
+            } else {
+
+                int[][] taula  = new int[files][columnes];
+
+                UtilTaula.inicialitzaTaula(taula, 1);
+
+                String resultat = UtilTaula.taulaToString(taula);
+
+                System.out.println(resultat);
+
+                System.out.println();
+
+            }
+
+        }
+
+    }
+
+
+    public static boolean especificacioCorrecta(String especificacio) {
+
+        /* retorna true si l'especificació és de la forma n'x'm on n i m
+
+         * són dos strings d'un o dos dígits.  Exemple: "12x5" seria
+
+         * correcta, mentre que "x3454" no.  */
+
+        String[] coordenades = especificacio.split("x");
+
+        if (coordenades.length != 2) return false;
+
+        for (int i=0; i<coordenades.length; i++) {
+
+            if (! UtilString.esEnter(coordenades[i])) return false;
+
+            int valor = Integer.parseInt(coordenades[i]);
+
+            if (valor < 0 || valor > 99) return false;
+
+        }
+
+        return true;
+
+    }
+
+
+    public static int obteFiles(String especificacio) {
+
+        /* Aquesta funció espera l'especificació de la forma d'una taula.
+
+         * En cas que l'especificació sigui correcta, retornarà l'enter
+
+         * corresponent als primers dígits. Per exemple, si especificacio
+
+         * és "12x5", retornarà 12.
+
+         * Altrament, retornarà el valor -1.  */
+
+        /*…*/
+        especificacio= especificacio.trim();
+        boolean ns= false;
+        for(int n=0; n<especificacio.length(); n++){
+            if(especificacio.charAp(n)=='x' || especificacio.charAp(n)=='X' ){
+                ns=true;
+            }
+        }
+        
+        if(Character.isDigit(especificacio.charAp(0)) || !ns){
+            return -1;
+        }
+        String[] coordenades = especificacio.split("x");
+        int[] files = Integer.parseInt(coordenades[0]);
+        return files;
+
+    }
+
+
+    public static int obteColumnes(String especificacio) {
+
+        /* Aquesta funció espera l'especificació de la forma d'una taula.
+
+         * En cas que l'especificació sigui correcta, retornarà l'enter
+
+         * corresponent als segons dígits. Per exemple, si especificacio
+
+         * és "12x5", retornarà 5.
+
+         * Altrament, retornarà el valor -1.  */
+
+        /*…*/
+        especificacio= especificacio.trim();
+        boolean ns= false;
+        for(int n=0; n<especificacio.length(); n++){
+            if(especificacio.charAp(n)=='x' || especificacio.charAp(n)=='X' ){
+                ns=true;
+            }
+        }
+        
+        if(Character.isDigit(especificacio.charAp(0)) || !ns){
+            return -1;
+        }
+        String[] coordenades = especificacio.split("x");
+        int[] files = Integer.parseInt(coordenades[1]);
+        return files;
+
+    }
+
+}
