fb73c8aa579c0b0d90e2bdd8d906927599b437fc
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-01-19 12:29:50 +0100

    1

--- 32_52_suma_digits/SumaDigits.java
+++ 32_52_suma_digits/SumaDigits.java
@@ -1,37 +1,22 @@
 /* 
  * Desenvolupa un programa que demani un text i mostri la suma dels dígits que conté.
  */
-
 public class SumaDigits {
-
     public static void main(String[] args){
-
         System.out.println("Text?");
-
         String text = Entrada.readLine();
-
         int nombres = sumaDigits(text);
-
         System.out.println(nombres);
-
     }
 
-
     // Suma numes els digits
-
     public static int sumaDigits(String text) {
-
         // cas base
-
         if ( text.isEmpty()) {      // cas base
             return 0;
         }
-
-
         // tracta pas actual
-
         char primer = text.charAt(0);
-        
         int numeroPrimerCaracter=0;
         String lletra="";
         if (Character.isDigit(primer)) {
@@ -39,12 +24,9 @@ public class SumaDigits {
             numeroPrimerCaracter=Integer.parseInt(lletra);
         }
         // tracta pas recursiu
-
         String restaText =text.substring(1);  // resta del text
 
-
         // composa resultat
-
         int numerosRestaText = sumaDigits(restaText);
         int total = numeroPrimerCaracter + numerosRestaText;
         return total;
