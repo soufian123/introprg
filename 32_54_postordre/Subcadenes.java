2cdc158ce8a0516ee790d2c040dbc0a09cf3b91c
prgtest <moises.gomez@iesjoandaustria.org>
2023-01-20 15:33:51 +0100

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 32_54 #0

--- 32_54_postordre/Subcadenes.java
+++ 32_54_postordre/Subcadenes.java
@@ -9,7 +9,7 @@ public class Subcadenes {
         System.out.println("Text?");
         String text = Entrada.readLine();
         text = borrar(text); 
-        imprimir(text, 0);
+        imprimir(text, text.length());
     }
 // borra els caràcters que no siguin lletres
     public static String borrar(String text) {
@@ -28,12 +28,11 @@ public class Subcadenes {
             System.out.printf("%4d: %s%n", length, text);
             return;
         }
-        System.out.printf("%4d: %s%n", length, text);
+        
         int mid = text.length() / 2;
-
         imprimir(text.substring(0, mid), mid);
         imprimir(text.substring(mid), text.length() - mid);
-        
+        System.out.printf("%4d: %s%n", length, text);
     }
 }
 
