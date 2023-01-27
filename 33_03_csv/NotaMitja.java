56cb0353893daef9600092dafd1931e0a4744629
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-01-27 15:57:45 +0100

    Valors separats per comes  ∾ prgtest autocommit 33_03 #1

--- 33_03_csv/NotaMitja.java
+++ 33_03_csv/NotaMitja.java
@@ -13,14 +13,18 @@ public class NotaMitja {
           String[] elements = linia.split(",");
           int numeros=0;
           int dividir=0;
+          int mitjana=0;
           for (int i=1; i<elements.length; i++) {
               if(Character.isDigit(elements[i].charAt(0))){
                   numeros= numeros + elements[i].charAt(0);
                   dividir++;
               }
           }
-          
-          int mitjana= numeros/dividir;
+          if (dividir==0 || numeros==0){
+            mitjana=0;
+            }else{
+                mitjana= numeros/dividir;
+            }
           System.out.println(elements[0]+ "("+mitjana+")");
      }
 }
