946e7be4c55c23d4226e820a22af9834fe0d0e1f
prgtest <moises.gomez@iesjoandaustria.org>
2023-01-31 18:26:35 +0100

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 33_03 #0

--- 33_03_csv/NotaMitja.java
+++ 33_03_csv/NotaMitja.java
@@ -9,39 +9,38 @@ import java.io.IOException;
 
 
 public class NotaMitja {
-     public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        
      
-     
-          String cami = "notes.csv";
-          FileReader fileReader = new FileReader(cami);
-          BufferedReader input = new BufferedReader(fileReader);
-          
-          int numeros=0;
-          double mitjana=0;
-          int numero=0;
-          while (true) {
-          String linia = input.readLine();
+        String cami = "notes.csv";
+        FileReader fileReader = new FileReader(cami);
+        BufferedReader input = new BufferedReader(fileReader);
+        
+        int numeros=0;
+        double mitjana=0;
+        int numero=0;
+        int intro=0;
+        String linia = input.readLine();
+
+        
+        while (true) {
+            linia = input.readLine();
             if (null == linia) break;
-
-            if(numero==0){
-                numero=1;
-            }else{
+            String[] elements = linia.split(",");
+            
+            for (int i=1; i<7; i++) {
+                if(UtilString.esEnter(elements[i])){
                 
-                
-                String[] elements = linia.split(",");
-              for (int i=1; i<7; i++) {
-                  if(Character.isDigit(elements[i].charAt(0))){
-                      numeros= numeros + elements[i].charAt(0);
+                    intro= Integer.parseInt(elements[i]);
+                    numeros= numeros + intro;
                     System.out.println(numeros);
-                  }else{
-
-                    mitjana= numeros/6;
                 }
-              }
-              System.out.println(elements[0]+ "("+mitjana+")");
-               numeros=0;
-               mitjana=0;
-         }
-         }
+            }
+            mitjana= numeros/6;
+            System.out.println(elements[0]+ "("+mitjana+")");
+            numeros=0;
+            mitjana=0;
+
+        }
      }
 }
