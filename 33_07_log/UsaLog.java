c657a4e0d97aa7b853be0ad3c2b825d6864d84f8
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-02-04 14:22:04 +0100

    log  ∾ prgtest autocommit 33_07 #1

--- 33_07_log/UsaLog.java
+++ 33_07_log/UsaLog.java
@@ -7,18 +7,34 @@
  
  
  
- 
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
 public class UsaLog {
-   public static void main(String[] args) throws IOException {
-       Log.printError("Això és un error greu");
-       Log.printWarning("Això és un avís");
-       for (int i=0; i < args.length; i++) {
-           Log.printInfo(String.format("Argument %d: %s", i, args[i]));
-       }
-       Log.reset();     // comencem a comptar un altre cop
-       Log.printDebug("Aquí s'acaba el main()");
-   }
+    public static void main(String[] args) throws IOException {
+   
+   
+   
+        String cami ="log.txt";
+        BufferedWriter sortida = new BufferedWriter(new FileWriter(cami, true));
+
+
+        Log.printError("Això és un error greu");
+        sortida.newLine();
+        Log.printWarning("Això és un avís");
+        sortida.newLine();
+        for (int i=0; i < args.length; i++) {
+            sortida.write(Log.printInfo(String.format("Argument %d: %s", i, args[i])));
+            sortida.newLine();
+        }
+
+
+
+        Log.reset();     // comencem a comptar un altre cop
+        sortida.write(Log.printDebug("Aquí s'acaba el main()"));
+        sortida.newLine();
+        sortida.close();
+    }
 }
 
 
