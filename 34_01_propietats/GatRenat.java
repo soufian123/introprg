ae7073695617b9813d6f1f5af70f516b8cc6436c
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-02-09 19:49:00 +0100

    fet

--- 34_01_propietats/GatRenat.java
+++ 34_01_propietats/GatRenat.java
@@ -10,3 +10,12 @@
 
 
 
+
+ 01   public class GatRenat {
+02       public int vides = 7;         // vides disponibles del gat Renat
+03       public static void main(String[] args) {
+04           GatRenat renat;           // declarem la referència al gat
+05           renat = new GatRenat();   // creem la instància del gat Renat.
+06           System.out.println("Al gat Renat li queden " + renat.vides + " vides");
+07       }
+08   }
