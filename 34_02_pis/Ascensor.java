6135d96e0ac23bda71649855b0e9bc653f9f59ae
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-02-09 19:55:42 +0100

    Ascensor  ∾ prgtest autocommit 34_02 #2

--- 34_02_pis/Ascensor.java
+++ 34_02_pis/Ascensor.java
@@ -6,8 +6,8 @@ public class Ascensor{
     public int planta=-1;
     public static void main (String[] args){
 
-        Ascensor soufi= new Ascensor();
-        System.out.println("L'ascensor està a la planta "+ soufi.planta);
+        Ascensor pis= new Ascensor();
+        System.out.println("L'ascensor està a la planta "+ pis.planta);
         
     }
 }
