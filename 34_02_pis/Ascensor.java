784e991bf13faf73185e672ab1216ecadc7e28d7
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-02-09 19:53:47 +0100

    Ascensor

---
+++ 34_02_pis/Ascensor.java
@@ -0,0 +1,13 @@
+//programa que es un ascensor i ens dira en quina planta estem
+
+
+
+public class Ascensor{
+    public int planta=-1;
+    public static void main (String[] args){
+
+        Ascensor soufi= new Ascensor();
+        System.out.println(" L'ascensor està a la planta "+ soufi.planta);
+        
+    }
+}
