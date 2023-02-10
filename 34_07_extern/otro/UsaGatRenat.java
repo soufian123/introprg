e937af7b842ff52f57e7073a70a471e38b7ff4a0
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-02-10 15:46:17 +0100

    fet

---
+++ 34_07_extern/otro/UsaGatRenat.java
@@ -0,0 +1,6 @@
+      public class UsaGatRenat {
+          public static void main(String[] args) {
+              GatRenat renat = new GatRenat();
+              System.out.println("Al gat Renat li queden " + renat.vides + " vides");
+              }
+      }
