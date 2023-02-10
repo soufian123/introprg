91797b2af2aaa8c8a5a716be06c62b549370c30f
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-02-10 16:14:55 +0100

    ascensor

---
+++ 34_08_desdefora/Ascensor.java
@@ -0,0 +1,8 @@
+//lloc on es crea el ascensor amb la seva posicio
+
+
+
+public class Ascensor {
+    public int pis = -1;  
+   
+}
---
+++ 34_08_desdefora/UsaAscensor.java
@@ -0,0 +1,13 @@
+
+
+//aquest es un programa que et diu en quin pis esta desde un ascensor que esta creat desde fora
+
+
+
+
+public class UsaAscensor {
+    public static void main(String[] args) {
+        Ascensor ascensor = new Ascensor();
+        System.out.println("L'ascensor creat des de fora està al pis " + ascensor.pis);
+        }
+}
