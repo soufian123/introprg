4437a469f5cb684c292349d3e711ec09eaa71c9f
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-02-14 16:37:36 +0100

    Ascensor protegit

---
+++ 34_13_ascensor_protegit/Ascensor.java
@@ -0,0 +1,32 @@
+/*aquest es el programa del ascensor on tenim tota la seva informacio guardada*/
+
+
+
+   public class Ascensor {
+       private int pis = -1;
+       private String moviment = "aturat";
+
+       public int getPis() {  
+           return pis;
+       }
+       public void setPis(int novespis) {   
+           if (novespis<11 &  novespis >-2){
+               pis = novespis;
+           }
+       }
+       public void setMoviment(String nouMoviment) {  
+           if (nouMoviment.equals("aturat") || nouMoviment.equals("pujant") || nouMoviment.equals("baixant")) {
+               moviment = nouMoviment;
+           }
+       }
+       
+       public String getMoviment() {  
+           return moviment;
+       }
+       
+       public static void main(String[] args) {
+           Ascensor ascensor = new Ascensor();
+
+
+       }
+   }
---
+++ 34_13_ascensor_protegit/UsaAscensor.java
@@ -0,0 +1,31 @@
+ /*programa que diu el pis inicial el moviment, el nou pis i dades del ascensor*/
+ 
+ 
+ 
+ public class UsaAscensor {
+
+     /* XXX */
+
+     public static void main(String[] args) {
+
+         Ascensor ascensor = new Ascensor();
+
+         System.out.println("Pis inicial: " + ascensor.getPis());
+
+         System.out.println("Moviment inicial: " + ascensor.getMoviment());
+
+         System.out.println("Introdueix nou pis:");
+
+         ascensor.setPis(Integer.parseInt(Entrada.readLine()));
+
+         System.out.println("Introdueix nou moviment:");
+
+         ascensor.setMoviment(Entrada.readLine());
+
+         System.out.println("Pis final: " + ascensor.getPis());
+
+         System.out.println("Moviment final: " + ascensor.getMoviment());
+
+     }
+
+ }
