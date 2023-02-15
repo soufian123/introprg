5345f2d8f06dfd403edaaec5444e2987f5354b5a
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-02-15 18:52:49 +0100

    ascensor informatiu  ∾ prgtest autocommit 34_15 #2

--- 34_15_ascensor_informatiu/UsaAscensor.java
+++ 34_15_ascensor_informatiu/UsaAscensor.java
@@ -4,12 +4,17 @@
 public class UsaAscensor {
     // XXX considera si et cal algun mòdul d'ajut
     public static void main(String[] args) {
+        int num=-1;
         Ascensor ascensor = new Ascensor();
         System.out.println("Pis inicial: " + ascensor.getPis());
         System.out.println("Moviment inicial: " + ascensor.getPosicio());
         System.out.println("Introdueix nou pis:");
         // XXX llegir el nou pis de stdin i assignar-lo a l'ascensor
-        ascensor.setPisFin(Integer.parseInt(Entrada.readLine()));
+        String text=Entrada.readLine();
+         if(Character.isDigit(text.charAt(0))){
+            num= Integer.parseInt(text);
+         }
+        ascensor.setPisFin(num);
         
         System.out.println("Introdueix nou moviment:");
         // XXX llegir el nou moviment de stdin i assignar-lo a l'ascensor
