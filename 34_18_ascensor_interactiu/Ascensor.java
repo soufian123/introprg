c8a5dc97698d0d137dfb12aa495231117aa35c5e
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-02-16 16:20:18 +0100

    ascensor interectiu

---
+++ 34_18_ascensor_interactiu/Ascensor.java
@@ -0,0 +1,83 @@
+/*
+ * programa que guarda la informacio de l'ascensor i li donem la informacio 
+ * que necesita el demanador(UsaAscensor)
+ *
+ */
+ 
+
+
+
+   public class Ascensor {
+       private int pis = -1;
+       private String moviment = "aturat";
+       
+       
+       public String comEsta (){
+            return (moviment+" al pis "+pis);
+       }
+        public int getPis() {  
+           return pis;
+       }
+       public String getMoviment() {  
+           return moviment;
+       }
+        
+        public void setMoviment(String novaMoviment) { 
+           if (novaMoviment.equals("abaix") || novaMoviment.equals("amunt") || novaMoviment.equals("aturat") || novaMoviment.equals("movent") || novaMoviment.equals("pujant") || novaMoviment.equals("baixant")) {
+               moviment = novaMoviment;
+           }
+           
+       }
+       public boolean estaAbaix(){
+            if(getPis()==-1){
+                return true;
+           }
+           return false;
+       }
+      public boolean estaAmunt(){
+           if(getPis()==10){
+                return true;
+           }
+           return false;
+        }
+      public boolean estaAturat(){
+           if(getMoviment().equals("aturat")){
+                return true;
+           }
+            return false;
+        }
+        public boolean estaMovent(){
+           if(estaPujant() || estaBaixant()){
+            return true;
+            }else{
+            return false;
+            }
+        }
+        public boolean estaPujant(){
+           if(getMoviment().equals("pujant")){
+                return true;
+           }
+            return false;
+        }
+       public boolean estaBaixant(){
+           if(getMoviment().equals("baixant")){
+                return true;
+           }
+            return false;
+        }
+        
+       public void setPis(int novespis) {   
+           if (novespis<11 &  novespis >-2){
+               pis = novespis;
+           }
+       }
+       
+       public static void main(String[] args) {
+           Ascensor ascensor = new Ascensor();
+
+
+       }
+   }
+
+
+
---
+++ 34_18_ascensor_interactiu/UsaAscensor.java
@@ -0,0 +1,41 @@
+
+/*
+ * programa que llegira la informacio que li demanem del programa ascensor
+ *
+ *
+ */
+
+
+ public class UsaAscensor {
+     public static int llegeixEnter() {
+         String resposta = Entrada.readLine();
+         if (! UtilString.esEnter(resposta)) {
+             return -2;
+         }
+         return Integer.parseInt(resposta);
+     }
+     public static void main(String[] args) {
+         Ascensor ascensor = new Ascensor();
+         System.out.println("Pis inicial");
+         ascensor.setPis(llegeixEnter());
+         System.out.printf("Inicialment l'ascensor està %s%n", ascensor.XXX);
+
+         // aturat a pujant
+         boolean resposta = ascensor.arrencaAmunt();
+         System.out.printf("En arrencar amunt l'ascensor respon %b i està %s%n",
+                 resposta, ascensor.XXX);
+         // demanem que baixi sense aturar
+         resposta = ascensor.arrencaAbaix();
+         System.out.printf("En arrencar a baix l'ascensor respon %b i està %s%n",
+                 resposta, ascensor.XXX);
+
+         // passem del pis indicat al la planta baixa
+         System.out.println("Anant a la planta baixa quan està " + ascensor.comEsta());
+         while (! ascensor.estaAbaix()) {
+             ascensor.XXX;       // passa al següent pis
+             System.out.println("\t ara està "+ ascensor.XXX);
+         }
+         ascensor.aturat();
+         System.out.printf("Està %s. Final de trajecte!%n", ascensor.comEsta());
+     }
+ }
