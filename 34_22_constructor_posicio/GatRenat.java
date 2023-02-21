a9ac57f7b9003a1283e16cd0da28390188a3062e
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-02-21 16:12:23 +0100

    Posició protegida en néixer

---
+++ 34_22_constructor_posicio/GatRenat.java
@@ -0,0 +1,34 @@
+/* 
+ * 
+ * Posició protegida en néixer, aquest programa es el gat renat, que en dira la seva informacio
+ * 
+ * 
+ */
+
+
+
+
+
+public class GatRenat {
+    private int vides = 7;
+    private String posicio="estirat";
+    public GatRenat(int vides, String posicio) {
+        setVides(vides);
+        setPosicio(posicio);
+    }
+    public int getVides() { return vides; }
+    public String getPosicio() { return posicio; }
+    public void setVides(int novesVides) {
+        if (novesVides >= 0)  {
+            vides = novesVides;
+        }
+    }
+    public void setPosicio(String novaPosicio) {
+        posicio=novaPosicio;
+    }
+    @Override
+    public String toString() { return String.format("Vides: %d. Posició: s%", vides, posicio); }
+    public static void main(String[] args) {
+        System.out.println(new GatRenat(7, "dret"));
+    }
+}
