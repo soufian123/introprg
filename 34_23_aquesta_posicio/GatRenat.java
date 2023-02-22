b97e7b3421a1a640922c5a40636262ddfa6bbb89
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-02-22 19:27:08 +0100

    posicio  ∾ prgtest autocommit 34_23 #16

--- 34_23_aquesta_posicio/GatRenat.java
+++ 34_23_aquesta_posicio/GatRenat.java
@@ -21,7 +21,7 @@ public class GatRenat {
         }
     }
     public void setPosicio(String posicio) {
-        if (posicio.equals("estirat")){
+        if (!posicio.equals("estirat")){
             this.posicio=posicio;
         }
     }
