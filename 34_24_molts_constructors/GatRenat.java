fab5a601b969dac7ea97617e188b25bcf405537c
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-02-23 00:23:57 +0100

    Molts constructors

---
+++ 34_24_molts_constructors/GatRenat.java
@@ -0,0 +1,59 @@
+/* 
+ * 
+ * Molts constructors, aquest programa es el gat renat, que ens dira la seva informacio
+ * 
+ * 
+ */
+
+
+public class GatRenat {
+    private int vides = 7;
+    private String posicio="estirat";
+    public GatRenat() {
+        setVides(vides);
+        setPosicio(posicio);
+    }
+    public GatRenat(int vides) {
+        setVides(vides);
+        setPosicio(posicio);
+    }
+    public GatRenat(String posicio) {
+        setPosicio(posicio);
+        setVides(vides);
+    }
+    public GatRenat(int vides, String posicio) {
+        setVides(vides);
+        setPosicio(posicio);
+    }
+    public int getVides() { return vides; }
+    public String getPosicio() { return posicio; }
+    public void setVides(int novesVides) {
+        if (novesVides >= 0)  {
+            vides = novesVides;
+        }
+    }
+    public void setPosicio(String posicio) {
+        if (posicio.equals("dret")){
+            this.posicio=posicio;
+        }else{
+            this.posicio="estirat";
+        }
+    }
+    @Override
+    public String toString() { 
+        return String.format("Vides: %d. Posició: %s", vides, posicio); 
+    }
+    
+    public static void main(String[] args) {
+        GatRenat[] renats = {
+            new GatRenat(),         // tot per defecte
+            new GatRenat(8),        // 8 vides i posició per defecte
+            new GatRenat("dret"),   // posició dret i vides per defecte
+            new GatRenat(8, "dret") // 8 vides i posició dret
+
+        };
+        for (GatRenat renat: renats) {
+            System.out.println(renat);
+        }
+    }
+}
