26dd0e63fc76248c4b3e87075bf82a77e3de4896
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-03-09 20:40:53 +0100

    fet

--- 34_31_exemple_herencia/Felí.java
+++ 34_31_exemple_herencia/Felí.java
@@ -1,28 +1,50 @@
 
 
+// Clase Felí, que hereda de Carnívor
 public class Felí extends Carnívor {
-    
     private UrpaRetractil[] urpes;
     public void caça(Animal presa) { /* … */ }
     
 }
-
-class Carnívor{
-    private String nom="Carnívor";
-    public void main(String[] args) {
-        System.out.println("Hola, soc en "+nom);
+// Clase Animal, clase pare de Felí
+public class Animal {
+    private String nom = "Animal";
+    
+    protected void main(String[] args) {
+        System.out.println("Hola, soc l\' " + nom);
     }
 }
 
-class Gat extends Felí{
-    private String nom="Gat";
-    public void main(String[] args) {
-        System.out.println("Hola, soc en "+nom);
+// Clase Carnívor, que es la clase pare de Felí
+class Carnívor {
+    
+    private String nom = "Carnívor";
+    
+    protected void main(String[] args) {
+        System.out.println("Hola, soc en " + nom);
     }
+    
 }
-class Pantera extends Felí{
-    private String nom="Pantera";
+
+// Clase Gat, que hereda de Felí
+class Gat extends Felí {
+    
+    private String nom = "Gat";
+    
     public void main(String[] args) {
-        System.out.println("Hola, soc en "+nom);
+        System.out.println("Hola, soc en " + nom);
     }
+    
 }
+
+// Clase Pantera, que hereda de Felí
+class Pantera extends Felí {
+    
+    private String nom = "Pantera";
+    
+    public void main(String[] args) {
+        System.out.println("Hola, soc en " + nom);
+    }
+    
+}
+
