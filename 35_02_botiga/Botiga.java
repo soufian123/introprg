964064ee3eafe5c3b48ea90d1777bb0e2cad45eb
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-03-30 19:11:06 +0200

    fet

---
+++ 35_02_botiga/Botiga.java
@@ -0,0 +1,80 @@
+/*
+ * aquest programa o archiu es la botiga, la botiga gestiona i en te tot els seu inventari
+ * on un dels seus inventaris es el vi, gestionarem la botiga i posare i eliminarem 
+ * els productes que volguem, en aquest cas es el vi.
+ *
+ */
+
+
+
+public class Botiga{
+    private int DEFAULT_MAX_VINS=10;
+    private Vi[] vins;
+    
+    public Botiga(){
+    vins = new Vi[DEFAULT_MAX_VINS];
+    }
+    public Botiga(int maxVins){
+    vins = new Vi[maxVins];
+    }
+    
+    public Vi afegeix(Vi vi){
+        if (!esta(vi)) {
+            for (int v = 0; v < vins.length; v++) {
+                if (vins[v] == null) {
+                    vins[v] = vi;
+                    return vi;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public boolean esta(Vi vi){
+        if (vi.esValid()){
+            for (Vi v: vins){
+                if (v != null) {
+                    if (!(v.getNom().equals(vi.getNom()))) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    
+    public Vi elimina(String nom){
+        nom= Vi.normalitzaNom(nom);
+        for (int v = 0; v < vins.length; v++) {
+            if (vins[v] != null) {
+                if (vins[v].getNom().equals(nom)) {
+                    if (vins[v].getEstoc() > 0) {
+                        return null;
+                    } else {
+                        Vi vi = vins[v];
+                        vins[v] = null;
+                        return vi;
+                    }
+                }
+            }
+        }
+        return null;
+        
+    }
+    
+    
+    public Vi cerca(String nom){
+        nom = Vi.normalitzaNom(nom);
+        for (int v = 0; v < vins.length; v++) {
+            if (vins[v] != null) {
+                String vi = vins[v].getNom().toLowerCase();
+                if (vi.equals(nom)) {
+                    return vins[v];
+                }
+            }
+        }
+        return null;
+    }
+
+}
