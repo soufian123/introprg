51d098fdcd6c79c923103bf8e63c9a2049b7b5df
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-04-10 16:44:00 +0200

    fet

---
+++ 35_05_vi_ampliat/Botiga.java
@@ -0,0 +1,126 @@
+/*
+ * aquest programa o archiu es la botiga, la botiga gestiona v en te tot els seu inventari
+ * on un dels seus inventaris es el vi, gestionarem la botiga v posare v eliminarem 
+ * els productes que volguem, en aquest cas es el vi.
+ *
+ */
+
+
+
+public class Botiga{
+    private int DEFAULT_MAX_VINS=10;
+    private Vi[] vins;
+    private int contador = -1;
+    
+    public Botiga(){
+    vins = new Vi[DEFAULT_MAX_VINS];
+    }
+    public Botiga(int maxVins){
+    vins = new Vi[maxVins];
+    }
+    
+    
+    public Vi afegeix(Vi vi) {
+        boolean esta = true;
+        if (vi.esValid()) {
+            for (int v = 0; v < vins.length; v++) {
+                if (vins[v] != null) {
+                    if (vins[v].getNom().equals(vi.getNom())) {
+                        esta = false;
+                    }
+                }
+            }
+            if (esta) {
+                for (int v = 0; v < vins.length; v++) {
+                    if (vins[v] == null) {
+                        vins[v] = vi;
+                        return vi;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    
+    public Vi elimina(String ref) {
+        ref = Vi.normalitzaNom(ref);
+        for (int v = 0; v < vins.length; v++) {
+            if (vins[v] != null) {
+                if (vins[v].getRef().equals(ref)) {
+                    if (vins[v].getEstoc() > 0) {
+                        return null;
+                    } else {
+                        Vi vi = vins[v];
+                        vins[v] = null;
+                        return vi;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    
+    
+    public Vi cerca(String ref){
+        ref = Vi.normalitzaNom(ref).toLowerCase();
+        for (int v = 0; v < vins.length; v++) {
+            if (vins[v] != null) {
+                String vi = vins[v].getRef().toLowerCase();
+                if (vi.equals(ref)) {
+                    return vins[v];
+                }
+            }
+        }
+        return null;
+    }
+    public Vi cerca (Vi plantilla) {
+        for(Vi vi: vins) {
+            if(vi == null) continue; 
+            if(!(plantilla.getRef()==null) && !(plantilla.getRef().equalsIgnoreCase(vi.getRef()))) continue;
+            if(!(plantilla.getNom()==null) && !(plantilla.getNom().equalsIgnoreCase(vi.getNom()))) continue;
+            if((plantilla.getPreu()!=-1) && (plantilla.getPreu() < vi.getPreu())) continue;
+            if((plantilla.getEstoc()!=-1) && (plantilla.getEstoc() > vi.getEstoc())) continue;
+            if(!(plantilla.getLloc() ==null) && !(plantilla.getLloc().equalsIgnoreCase(vi.getLloc()))) continue;
+            if(!(plantilla.getOrigen()==null) && !(plantilla.getOrigen().equalsIgnoreCase(vi.getOrigen()))) continue;
+            if(!(plantilla.getTipus()==null) && !(plantilla.getTipus().equalsIgnoreCase(vi.getTipus()))) continue;
+            if(!(plantilla.getCollita()==null) && !(plantilla.getCollita().equalsIgnoreCase(vi.getCollita()))) continue;
+            return vi;
+        }
+        return null;
+    }
+    public Vi cerca() {
+        if(vins[0]!=null) return vins[0];
+        return null;
+    }
+
+    
+
+
+    public int getContador() {
+        return this.contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+     public void iniciaRecorregut() {
+        setContador(-1);
+    }
+
+
+    public Vi getSeguent() {
+        if (getContador() < vins.length) {
+            setContador(getContador() + 1);
+        }
+        if (getContador() == vins.length) {
+            return null;
+        }
+
+        while (vins[getContador()] == null && getContador() < vins.length - 1) {
+            setContador(getContador() + 1);
+        }
+
+        return vins[getContador()];
+    }
+
+}
---
+++ 35_05_vi_ampliat/Vi.java
@@ -0,0 +1,161 @@
+
+/*
+ * programa o archiu que el que fa es que gestionar el nom, el preu i el estoc
+ * estiguin ven escrits, per exemple que el nom no tingui espais o altres coses
+ * li posem nom als vins, estoc i preu
+ *
+ */
+
+
+
+
+
+
+public class Vi{
+    private String nom="Roura blanc";
+    private int preu=-1;
+    private int estoc=-1;
+    private String ref;
+    private String lloc;
+    private String origen;
+    private String tipus;
+    private String collita;
+    
+    public String getRef(){return ref;}
+    public void setRef(String ref){
+        this.ref=normalitzaNom(ref);
+    }
+    
+    
+    public String getLloc(){return lloc;}
+    public void setLloc(String lloc){
+        this.lloc=normalitzaNom(lloc);
+    }
+    
+    public String getOrigen(){return origen;}
+    public void setOrigen(String origen){
+        this.origen=normalitzaNom(origen);
+    }
+    
+    public String getTipus(){return tipus;}
+    public void setTipus(String tipus){
+        this.tipus=normalitzaNom(tipus);
+    }
+    
+    public String getCollita(){return collita;}
+    public void setCollita(String collita){
+        this.collita=normalitzaNom(collita);
+    }
+    
+    public Vi(String nom, int preu){
+        setNom(nom);
+        setPreu(preu);
+        setEstoc(0);
+    }
+    public Vi(String nom, int preu, int estoc){
+        setNom(nom);
+        setPreu(preu);
+        setEstoc(estoc);
+    }
+    public Vi( String ref,String nom, int preu, int estoc, String lloc, String origen, String tipus, String collita){
+        setNom(nom);
+        setPreu(preu);
+        setEstoc(estoc);
+        setRef(ref);
+        setLloc(lloc);
+        setOrigen(origen);
+        setTipus(tipus);
+        setCollita(collita);
+    }
+    
+    public String getNom(){
+        return nom;
+    }
+    public void setNom(String nom){
+        this.nom=normalitzaNom(nom);
+    }
+    
+    
+    public int getPreu(){
+        return preu;
+    }
+    public void setPreu(int preu){
+        if (preu>=0){
+            this.preu=preu;
+        }
+    }
+    
+    public int getEstoc(){
+        return estoc;
+    }
+    public void setEstoc(int estoc){
+        if (estoc<0){
+            //this.estoc=-1;
+            return;
+        }
+        this.estoc=estoc;
+    }
+    
+    
+    
+    public static String normalitzaNom(String nom){
+        nom=nom.trim();
+        if (!nom.isEmpty() || nom==null){
+            
+            nom= nom.replaceAll(" +"," ");
+            return nom;
+        }
+        return "NOM NO VÀLID!";
+    }
+    public static String normalitzaString(String text){
+        text=text.trim();
+        if (!text.isEmpty() || text==null){
+            
+            text= text.replaceAll(" +"," ");
+            return text;
+        }
+        return "NOM NO VÀLID!";
+    }
+    public boolean esValid(){
+        if(normalitzaNom(getNom()).equals("NOM NO VÀLID!")){ return false;}
+        if(getPreu()<0){return false;}
+        if(getEstoc()<0){return false;}
+        if(ref!=null && ref.length()>0 && lloc!=null && lloc.length()>0 && origen!=null && origen.length()>0 && tipus!=null && tipus.length()>0 && collita!=null && collita.length()>0){return false;}
+        return true;
+    }
+    
+
+    @Override
+    public String toString() {
+        String text = String.format("\n    Ref: %s\n    Nom: %s\n    Preu: %s\n    Estoc: %s\n    Lloc: %s\n    D.O.: %s\n    Tipus: %s\n    Collita: %s\n",getRef(), getNom(),getPreu(),getEstoc(),getLloc(),getOrigen(),getTipus(),getCollita());
+        return text;
+    }
+    
+    public static Vi deArrayString(String[] vins) {
+        if(vins.length != 8) return null;
+        for(int a=0; a<8; a++){
+            if(a!=2 && a!=3){
+                if(vins[a].isBlank()|| vins[a]==null) return null;
+            }else{
+                if(!UtilString.esEnter(vins[a])|| vins[a]==null) return null;
+            }
+        }
+
+        Vi nou = new Vi(normalitzaString(vins[0]),normalitzaString(vins[1]),Integer.parseInt(vins[2]),Integer.parseInt(vins[3]),normalitzaString(vins[4]),normalitzaString(vins[5]),normalitzaString(vins[6]),normalitzaString(vins[7]));
+        if (nou.esValid()) {
+            return nou;
+        } else {
+            return null;
+        }
+    }
+
+
+
+    public String[] aArrayString() {
+        String[] viString ={ getRef(), getNom(),Integer.toString(getPreu()),Integer.toString(getEstoc()), getLloc(), getOrigen(), getTipus(), getCollita()};
+        return viString;
+        
+    }
+
+
+}
