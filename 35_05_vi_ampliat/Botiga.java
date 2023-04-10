de3d0f51ad9fa7e0a918558c8fa23a172f33eb79
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-04-10 19:51:14 +0200

    fet  ∾ prgtest autocommit 35_05 #60

--- 35_05_vi_ampliat/Botiga.java
+++ 35_05_vi_ampliat/Botiga.java
@@ -43,10 +43,10 @@ public class Botiga{
     }
     
     public Vi elimina(String ref) {
-        ref = Vi.normalitzaString(ref);
+        ref = Vi.normalitzaString(ref).toLowerCase();
         for (int v = 0; v < vins.length; v++) {
             if (vins[v] != null) {
-                if (vins[v].getRef().equals(ref)) {
+                if (vins[v].getRef().equals(ref.toLowerCase())) {
                     if (vins[v].getEstoc() > 0) {
                         return null;
                     } else {
