c6cade49f2530a8aeb02aa080e3e424eeb189de7
Soufian El Hajouji <cf22soufian.el@iesjoandaustria.org>
2023-04-10 21:29:03 +0200

    fet

--- 35_07_hores_excepcionals/UsaHora.java
+++ 35_07_hores_excepcionals/UsaHora.java
@@ -4,42 +4,44 @@
 public class UsaHora {
 
     private static void canviaHora(Hora hora, int hores, int minuts, int segons) {
-        Hora hora = new Hora();
-        hora.setHora(hora)
         
+        try {
+            hora.setHores(hores);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        try {
+            hora.setMinuts(minuts);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        try {
+            hora.setSegons(segons);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 
     public static void main(String[] args) {
 
         Hora hora = new Hora();
-
         System.out.println("Inicialment " + hora);
-
         System.out.println("Si intentem assignar a hores un -1");
-
         canviaHora(hora, -1, hora.getMinuts(), hora.getSegons());
-
         System.out.println("El valor resultant és " + hora);
-
         System.out.println();
-
 
         System.out.println("Si intentem assignar a minuts un -1");
-
         canviaHora(hora, hora.getHores(), -1, hora.getSegons());
-
         System.out.println("El valor resultant és " + hora);
-
         System.out.println();
 
-
         System.out.println("Si intentem assignar a segons un -1");
-
         canviaHora(hora, hora.getHores(), hora.getMinuts(), -1);
-
         System.out.println("El valor resultant és " + hora);
-
         System.out.println();
 
     }
