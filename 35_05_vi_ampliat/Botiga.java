72242de7bc1c08397f4bb8c606cae6022a6003d9
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-04-11 18:55:03 +0200

    fet  ∾ prgtest autocommit 35_05 #17

--- 35_05_vi_ampliat/Botiga.java
+++ 35_05_vi_ampliat/Botiga.java
@@ -1,10 +1,140 @@
+public class Botiga {
+    private int DEFAULT_MAX_VINS = 10;
+    private Vi[] vins;
+    private int contador = -1;
+
+    public Botiga() {
+        vins = new Vi[DEFAULT_MAX_VINS];
+        this.contador = -1;
+    }
+
+    public Botiga(int maxVins) {
+        vins = new Vi[maxVins];
+        this.contador = -1;
+    }
+
+    public Vi afegeix(Vi vi) {
+        boolean esta = true;
+        if (vi.esValid()) {
+            for (int v = 0; v < vins.length; v++) {
+                if (vins[v] != null) {
+                    if (vins[v].getNom().equals(vi.getNom())) {
+                        esta = false;
+                    }
+                }
+            }
+            if (esta) {
+                for (int v = 0; v < vins.length; v++) {
+                    if (vins[v] == null) {
+                        vins[v] = vi;
+                        return vi;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public Vi elimina(String ref) {
+        ref = Vi.normalitzaString(ref);
+        for (int v = 0; v < vins.length; v++) {
+            if (vins[v] != null) {
+                if (vins[v].getRef().equalsIgnoreCase(ref)) {
+                    if (vins[v].getEstoc() > 0) {
+                        return null;
+                    } else {
+                        Vi vi = vins[v];
+                        vins[v] = null;
+                        return vi;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public Vi cerca(String ref) {
+        ref = Vi.normalitzaString(ref).toLowerCase();
+        for (int v = 0; v < vins.length; v++) {
+            if (vins[v] != null) {
+                String vi = vins[v].getRef().toLowerCase();
+                if (vi.equals(ref)) {
+                    return vins[v];
+                }
+            }
+        }
+        return null;
+    }
+
+    public Vi cerca(Vi plantilla) {
+        for(Vi vi: vins) {
+            if(vi == null) continue; 
+            if(!(plantilla.getRef()==null) && !(plantilla.getRef().equalsIgnoreCase(vi.getRef()))) continue;
+            if(!(plantilla.getNom()==null) && !(plantilla.getNom().equalsIgnoreCase(vi.getNom()))) continue;
+            if((plantilla.getPreu()!=-1) && (plantilla.getPreu() < vi.getPreu())) continue;
+            if((plantilla.getEstoc()!=-1) && (plantilla.getEstoc() > vi.getEstoc())) continue;
+            if(!(plantilla.getLloc() ==null) && !(plantilla.getLloc().equalsIgnoreCase(vi.getLloc()))) continue;
+            if(!(plantilla.getOrigen()==null) && !(plantilla.getOrigen().equalsIgnoreCase(vi.getOrigen()))) continue;
+            if(!(plantilla.getTipus()==null) && !(plantilla.getTipus().equalsIgnoreCase(vi.getTipus()))) continue;
+            if(!(plantilla.getCollita()==null) && !(plantilla.getCollita().equalsIgnoreCase(vi.getCollita()))) continue;
+            return vi;
+        }
+        
+        return null;
+    }
+
+    public Vi cerca() {
+        if (vins[0] != null)
+            return vins[0];
+        return null;
+    }
+    public int getContador() {
+        return this.contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+     public void iniciaRecorregut() {
+        setContador(-1);
+    }
+
+    public Vi getSeguent() {
+
+        while (true) {
+            contador++;
+            if (getContador() >= vins.length) return null;
+            
+            if (vins[getContador()] == null) continue;
+            return vins[getContador()];
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
  * aquest programa o archiu es la botiga, la botiga gestiona v en te tot els seu inventari
  * on un dels seus inventaris es el vi, gestionarem la botiga v posare v eliminarem 
  * els productes que volguem, en aquest cas es el vi.
  *
  */
-
+/*
 public class Botiga{
     private int DEFAULT_MAX_VINS=10;
     private Vi[] vins;
@@ -71,7 +201,7 @@ public class Botiga{
         }
         return null;
     }
-/*
+
     public Vi cerca(Vi plantilla) {
         for(Vi vi: vins) {
             if(vi == null) continue; 
@@ -86,21 +216,6 @@ public class Botiga{
             return vi;
         }
         
-        return null;
-    }*/
-    public Vi cerca(Vi plantilla) {
-        for(Vi vi: vins) {
-            if(vi == null) continue; 
-            if(!(plantilla.getRef()==null) && !(plantilla.getRef().equalsIgnoreCase(vi.getRef()))) continue;
-            if(!(plantilla.getNom()==null) && !(plantilla.getNom().equalsIgnoreCase(vi.getNom()))) continue;
-            if((plantilla.getPreu()!=-1) && (plantilla.getPreu() < vi.getPreu())) continue;
-            if((plantilla.getEstoc()!=-1) && (plantilla.getEstoc() > vi.getEstoc())) continue;
-            if(!(plantilla.getLloc() ==null) && !(plantilla.getLloc().equalsIgnoreCase(vi.getLloc()))) continue;
-            if(!(plantilla.getOrigen()==null) && !(plantilla.getOrigen().equalsIgnoreCase(vi.getOrigen()))) continue;
-            if(!(plantilla.getTipus()==null) && !(plantilla.getTipus().equalsIgnoreCase(vi.getTipus()))) continue;
-            if(!(plantilla.getCollita()==null) && !(plantilla.getCollita().equalsIgnoreCase(vi.getCollita()))) continue;
-            return vi;
-        }
         return null;
     }
 
@@ -134,6 +249,6 @@ public class Botiga{
         }
     }
 }
-
+*/
 
 
