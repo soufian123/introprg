68b1814fd9e0df77c45184027fdf65dc90a0aa3e
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-04-17 17:19:07 +0200

    fet  ∾ prgtest autocommit 35_17 #2

--- 35_17_envoltastring_case/EnvoltaString.java
+++ 35_17_envoltastring_case/EnvoltaString.java
@@ -8,17 +8,11 @@
 
 public class EnvoltaString {
     private String valor;
+
     public EnvoltaString(String valor) { this.valor = valor; }
     public String toString() { return String.format("ENVOLTAT(\"%s\")", valor); }
-    public static void main(String[] args) {
-        String nom1 = "Renat";
-        String nom2 = new String("Renat");
-        System.out.printf("\"%s\" == \"%s\" -> %b%n", nom1, nom2, nom1 == nom2);
-        System.out.printf("\"%s\".equals(\"%s\") -> %b%n", nom1, nom2, nom1.equalsIgnoreCase(nom2));
 
-        EnvoltaString envoltaNom1 = new EnvoltaString(nom1);
-        EnvoltaString envoltaNom2 = new EnvoltaString(nom2);
-        System.out.printf("%s == %s -> %b%n", envoltaNom1, envoltaNom2, envoltaNom1 == envoltaNom2);
-        System.out.printf("%s.equals(%s) -> %b%n", envoltaNom1, envoltaNom2, envoltaNom1.equals(envoltaNom2));
+    public boolean equals(EnvoltaString altre) {
+        return valor.equalsIgnoreCase(altre.valor);
     }
 }
--- 35_17_envoltastring_case/UsaEnvoltaString.java
+++ 35_17_envoltastring_case/UsaEnvoltaString.java
@@ -7,13 +7,12 @@
 
 
 
-
 public class UsaEnvoltaString {
      public static void main(String[] args) {
          String nom1 = "Renat";
          String nom2 = args.length > 0 ? args[0] : "nom per defecte";
          System.out.printf("\"%s\" == \"%s\" -> %b%n", nom1, nom2, nom1 == nom2);
-         System.out.printf("\"%s\".equals(\"%s\") -> %b%n", nom1, nom2, nom1.equalsIgnoreCase(nom2));
+         System.out.printf("\"%s\".equals(\"%s\") -> %b%n", nom1, nom2, nom1.equals(nom2));
 
          EnvoltaString envoltaNom1 = new EnvoltaString(nom1);
          EnvoltaString envoltaNom2 = new EnvoltaString(nom2);
@@ -21,3 +20,4 @@ public class UsaEnvoltaString {
          System.out.printf("%s.equals(%s) -> %b%n", envoltaNom1, envoltaNom2, envoltaNom1.equals(envoltaNom2));
      }
  }
+
