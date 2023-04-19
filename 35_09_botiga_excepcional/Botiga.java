b381f4d2d9f38d85cf27bbddbf858477e1a70e88
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-04-19 23:29:01 +0200

    Merge branch 'main' of github.com:soufian123/introprg  ∾ prgtest autocommit 35_09 #11

--- 35_09_botiga_excepcional/Botiga.java
+++ 35_09_botiga_excepcional/Botiga.java
@@ -27,7 +27,7 @@ public class Botiga{
     
     public Vi afegeix(Vi vi) throws IllegalArgumentException, BotigaException{
         boolean esta = false;
-        if (contador >= vins.length) throw new BotigaException();
+        if (contador >= DEFAULT_MAX_VINS+1) throw new BotigaException();
         if (vi==null) throw new IllegalArgumentException("El vi no pot ser null");
         if (vi.esValid()) {
             for (int v = 0; v < vins.length; v++) {
