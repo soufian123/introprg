0e933d3916b8fb2a5a28b5e8fbb6e9607d4adfcd
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-04-20 00:36:07 +0200

    fet  ∾ prgtest autocommit 35_18 #3

--- 35_18_envoltastring_str/EnvoltaString.java
+++ 35_18_envoltastring_str/EnvoltaString.java
@@ -10,13 +10,15 @@ public class EnvoltaString {
     private String valor;
 
     public EnvoltaString(String valor) { this.valor = valor; }
-    public String toString() { return String.format("ENVOLTAT(\"%s\")", valor); }
+
 
     public boolean equals(EnvoltaString altre) {
         return valor.equalsIgnoreCase(altre.valor);
     }
     @Override
+     public String toString() { return String.format("ENVOLTAT(\"%s\")", valor); }
 
+    @Override
     public boolean equals(Object obj) {
         if (obj instanceof EnvoltaString) {  // si ja ni tant sols és ni un EnvoltaString malament
             EnvoltaString altre = (EnvoltaString) obj;
