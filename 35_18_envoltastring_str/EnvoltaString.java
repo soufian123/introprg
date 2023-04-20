9e74340ddb9309f71ad2c6fb059ef611999350ed
prgtest <cf22soufian.el@iesjoandaustria.org>
2023-04-20 12:35:18 +0200

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 35_18 #0

--- 35_18_envoltastring_str/EnvoltaString.java
+++ 35_18_envoltastring_str/EnvoltaString.java
@@ -18,7 +18,7 @@ public class EnvoltaString {
     }
 
     public boolean equals(Object o) {
-        if (o == this) {
+        if (o == valor) {
             return true;
         }
         if (!(o instanceof EnvoltaString)) {
