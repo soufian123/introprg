f0a696f7c77b5db2c5738eac0bed99e8a2a45a27
Soufian El Hajouji El Hayadi <proni10102002@gmail.com>
2023-05-02 18:15:11 +0200

    fet

---
+++ 52_04_setter_y/UsaPunt.java
@@ -0,0 +1,20 @@
+/* 
+ * Es el Usa Punt que el que fa es anar fent les crides necessaries o 
+ * Utilitzes com be diu el nom, el programa Punt perque ens vagi ensenyant 
+ * tota la informacio de la que li anem dient.
+ */
+
+
+
+public class UsaPunt {
+    public static void main(String[] args){
+        Punt punt = new Punt();
+        int x = // args[0] si hi és i és enter, o 0 altrament
+        int y = //  args[1] si hi és i és enter, o 0 altrament
+        punt.setX(x);
+        punt.setY(y);
+        System.out.printf("punt.getX() -> %d%n", punt.getX());
+        System.out.printf("punt.getY() -> %d%n", punt.getY());
+    }
+}
+
