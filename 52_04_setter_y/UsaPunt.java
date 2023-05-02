f1e83235d0bb499d33e0fa0e2f8b143b09b1340d
prgtest <proni10102002@gmail.com>
2023-05-02 18:40:28 +0200

    fet  ∾ prgtest autocommit 52_04 #3

--- 52_04_setter_y/UsaPunt.java
+++ 52_04_setter_y/UsaPunt.java
@@ -9,16 +9,22 @@
 public class UsaPunt {
     public static void main(String[] args){
         Punt punt = new Punt();
-        if (!UtilString.esEnter(args[0]) || args[0]==null){
-            args[0]="0";
-        }
-        if (!UtilString.esEnter(args[1]) || args[1]==null){
-            args[1]="0";
-        }
         
-        
-        int x = Integer.parseInt(args[0]); // args[0] si hi és i és enter, o 0 altrament
-        int y = Integer.parseInt(args[1]); //  args[1] si hi és i és enter, o 0 altrament
+        int x = 0; // args[0] si hi és i és enter, o 0 altrament
+        int y = 0; //  args[1] si hi és i és enter, o 0 altrament
+        if (args.length == 2) {
+            if (UtilString.esEnter(args[0])) {
+                x = Integer.parseInt(args[0]);
+            }
+            if (UtilString.esEnter(args[1])) {
+                y = Integer.parseInt(args[1]);
+            }
+        } else if (args.length == 1) {
+            if (UtilString.esEnter(args[0])) {
+                x = Integer.parseInt(args[0]);
+            }
+        }
+
         punt.setX(x);
         punt.setY(y);
         System.out.printf("punt.getX() -> %d%n", punt.getX());
