28b4b162a56e4b5f7efa45c36c4d47c51fd6afdd
Soufian El Hajouji El Hayadi <proni10102002@gmail.com>
2023-05-02 17:21:22 +0200

    fet

---
+++ 52_04_setter_y/TestPunt.java
@@ -0,0 +1,24 @@
+/* es el meu testejador del programa Punt, anira fent crides 
+ * i verificant la informacio del programa Punt amb el meu testejador
+ * o intentan fent totes les crides i que no falli.
+ * 
+*/
+
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class TestPunt {
+    @Test
+    public void constructor() {
+        // comprova que Punt() pugui ser cridat
+        new Punt();
+    }
+
+    @Test
+    public void constructorDefecteXZero() {
+        // comprova que Punt() deixi a 0 la propietat x de Punt
+        Punt p = new Punt();
+        assertEquals(0, p.getX());
+    }
+}
---
+++ 52_04_setter_y/UsaPunt.java
@@ -0,0 +1,15 @@
+/* 
+ * Es el Usa Punt que el que fa es anar fent les crides necessaries o 
+ * Utilitzes com be diu el nom, el programa Punt perque ens vagi ensenyant 
+ * tota la informacio de la que li anem dient.
+ */
+
+
+
+public class UsaPunt {
+    public static void main(String[] args){
+    Punt punt = new Punt();
+    System.out.printf("punt.getX() -> %d%n", punt.getX());
+    System.out.printf("punt.getY() -> %d%n", punt.getY());
+    }
+}
