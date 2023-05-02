a3f2502f4707fdb2885c94536b2a103cb8bb62b0
Soufian El Hajouji El Hayadi <proni10102002@gmail.com>
2023-05-02 17:24:27 +0200

    fet

--- 52_04_setter_y/TestPunt.java
+++ 52_04_setter_y/TestPunt.java
@@ -21,4 +21,10 @@ public class TestPunt {
         Punt p = new Punt();
         assertEquals(0, p.getX());
     }
+    @Test
+    public void constructorDefecteYZero() {
+        // comprova que Punt() deixi a 0 la propietat x de Punt
+        Punt p = new Punt();
+        assertEquals(0, p.getY());
+    }
 }
