40d29b9cca4327ab02fc07c52ac6d2721409b025
prgtest <proni10102002@gmail.com>
2023-05-05 16:39:09 +0200

    fet  ∾ prgtest autocommit 52_10 #1

--- 52_10_gestor_lloguers/Lloguer.java
+++ 52_10_gestor_lloguers/Lloguer.java
@@ -11,7 +11,7 @@
 import java.util.*;
 public class Lloguer {
     private int dies;
-    private Vehicle vehicle;;
+    private Vehicle vehicle;
     
     public Lloguer(Vehicle vehicle,int dies) {
         this.dies = dies;
