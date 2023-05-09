8627dff77bff04b0f0bc5da7707c606e03b454d5
prgtest <proni10102002@gmail.com>
2023-05-09 17:28:46 +0200

    fet  ∾ prgtest autocommit 52_14 #1

--- 52_14_mes_tests/TestGestor.java
+++ 52_14_mes_tests/TestGestor.java
@@ -5,28 +5,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class TestGestor {
-  
-  @Test
-  public void testInforme() {
-    // Crear una nova instància del client demo
-    Client client = GestorLite.creaClientDemo();
-    
-    // Definir el valor esperat
-    String valorEsperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n" +
-                          "\tSeat 600: 90.0€\n" +
-                          "\tTata Vista: 180.0€\n" +
-                          "\tFiat Uno: 540.0€\n" +
-                          "Import a pagar: 810.0€\n" +
-                          "Punts guanyats: 4\n";
 
-    
-    // Cridar informe() i guardar el seu resultat com a valor obtingut
-    String valorObtingut = client.informe();
-    
-    // Comparar el valor esperat amb el valor obtingut
-    assertEquals(valorEsperat, valorObtingut);
-  }
-  
+    @Test
+    public void testInforme() {
+        // Crear una nova instància del client demo
+        Client client = GestorLite.creaClientDemo();
+
+        // Definir el valor esperat
+        String valorEsperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n" +
+            "\tSeat 600: 90.0€\n" +
+            "\tTata Vista: 180.0€\n" +
+            "\tFiat Uno: 540.0€\n" +
+            "Import a pagar: 810.0€\n" +
+            "Punts guanyats: 4\n";
+
+
+        // Cridar informe() i guardar el seu resultat com a valor obtingut
+        String valorObtingut = client.informe();
+
+        // Comparar el valor esperat amb el valor obtingut
+        assertEquals(valorEsperat, valorObtingut);
+    }
+    @Test
+    public void cehicleBasic() {
+        Client client = new Client("51590695Q","Eugènia Salinas Roig","93614214242");     
+        client.addLloguers(new Lloguer(new Vehicle("Seat","600",Vehicle.BASIC),1));
+        
+        assertEquals(client.getLloguers().get(1), new Lloguer(new Vehicle("Seat","600",Vehicle.BASIC),1));
+    }
 }
 
 
