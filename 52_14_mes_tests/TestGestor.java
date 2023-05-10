261702a1a458ed0983d5aadc5b4b05eb9106f2b9
prgtest <proni10102002@gmail.com>
2023-05-10 15:50:54 +0200

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 52_14 #5

--- 52_14_mes_tests/TestGestor.java
+++ 52_14_mes_tests/TestGestor.java
@@ -3,7 +3,8 @@
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 public class TestGestor {
 
     @Test
