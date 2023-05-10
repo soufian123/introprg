d3966f3d7e3f21c7511937c4278bc283ec4780f5
Soufian El Hajouji El Hayadi <proni10102002@gmail.com>
2023-05-10 16:59:58 +0200

    fet

---
+++ 52_21_moviment/GestorLite.java
@@ -0,0 +1,43 @@
+/*
+ * es En aquest exercici codificaràs la classe GestorLite que et
+ * permetrà fer una prova bàsica del meu codi.
+ * Utilitzan les altres classes.
+ *
+ */
+ 
+
+
+
+class GestorLite{
+    private Client client;
+
+    public static void main(String[] args) {
+        Client demo = creaClientDemo();
+        mostraClient(demo);
+    }
+    public static Client creaClientDemo(){
+        Client client = new Client("51590695Q","Eugènia Salinas Roig","93614214242");     
+        client.addLloguers(new Lloguer(new Vehicle("Seat","600",Vehicle.BASIC),2));
+        client.addLloguers(new Lloguer(new Vehicle("Tata","Vista",Vehicle.BASIC),5));
+        client.addLloguers(new Lloguer(new Vehicle("Fiat","Uno",Vehicle.LUXE),3));
+        return client;
+    }
+    
+    public static void mostraClient(Client client){
+        System.out.println("Client: " + client.getNom());
+        System.out.println("        " + client.getNif());
+        System.out.println("        " + client.getTelefon());
+        System.out.println("Lloguers: " + client.getLloguers().size());
+        for (int a=0; a<client.getLloguers().size(); a++){
+            
+            System.out.println("        "+(a+1)+". vehicle: " + client.getLloguers().get(a).getVehicle().getMarca()+ " "+ client.getLloguers().get(a).getVehicle().getModel());
+            System.out.println("           dies llogats: " + client.getLloguers().get(a).getDies());
+        }
+    }
+
+}
+
+
+
+
+
