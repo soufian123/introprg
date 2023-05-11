202235b014155a927ee1b752575ea7a8062ebe0e
Soufian El Hajouji El Hayadi <proni10102002@gmail.com>
2023-05-11 19:53:06 +0200

    fet

--- 52_28_una_constant/Client.java
+++ 52_28_una_constant/Client.java
@@ -88,12 +88,12 @@ public class Client {
         }
         return bonificacions;
     }
-    public String composaCapsalera(){
+    private String composaCapsalera(){
         return "Informe de lloguers del client " +
             getNom() +
             " (" + getNif() + ")\n";
     }
-    public String composaDetall(){
+    private String composaDetall(){
         String resultat="";
         for (Lloguer lloguer: lloguers) {
 
@@ -106,7 +106,7 @@ public class Client {
         }
         return resultat;
     }
-    public String composaPeu(){
+    private String composaPeu(){
         return "Import a pagar: " + importTotal() + "€\n" +
             "Punts guanyats: " + bonificacionsTotal() + "\n";
     }
