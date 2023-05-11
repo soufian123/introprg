de7c8687f4dcf74f467e6104a8ebdf77994ae64f
Soufian El Hajouji El Hayadi <proni10102002@gmail.com>
2023-05-11 19:40:35 +0200

    fet

--- 52_29_mes_constants/Lloguer.java
+++ 52_29_mes_constants/Lloguer.java
@@ -26,36 +26,36 @@ public class Lloguer {
     private static final double QUANTITAT_GENERAL = 4;
     private static final double MULTIPLE_GENERAL = 2.5;
     private static final double MULTIPLE_LUXE = 6;
-    public double quantitat(Lloguer lloguer){
+    public double quantitat(){
         double quantitat = 0;
-        switch (lloguer.getVehicle().getCategoria()) {
+        switch (getVehicle().getCategoria()) {
             case Vehicle.BASIC:
                 quantitat += DIES_BASIC;
-                if (lloguer.getDies() > DIES_BASIC) {
-                    quantitat += (lloguer.getDies() - DIES_BASIC) * MULTIPLE_BASIC;
+                if (getDies() > DIES_BASIC) {
+                    quantitat += (getDies() - DIES_BASIC) * MULTIPLE_BASIC;
                 }
                 break;
             case Vehicle.GENERAL:
                 quantitat += QUANTITAT_GENERAL;
-                if (lloguer.getDies() > DIES_GENERAL) {
-                    quantitat += (lloguer.getDies() - DIES_GENERAL) * MULTIPLE_GENERAL;
+                if (getDies() > DIES_GENERAL) {
+                    quantitat += (getDies() - DIES_GENERAL) * MULTIPLE_GENERAL;
                 }
                 break;
             case Vehicle.LUXE:
-                quantitat += lloguer.getDies() * MULTIPLE_LUXE;
+                quantitat += getDies() * MULTIPLE_LUXE;
                 break;
         }
         
         return quantitat;
     }
-    public static int bonificacionsDeLloguer(Lloguer lloguer){
+    public static int bonificacionsDeLloguer(){
         int bonificacions=0;
         // afegeix lloguers freqüents
         bonificacions ++;
 
         // afegeix bonificació per dos dies de lloguer de Luxe
-        if (lloguer.getVehicle().getCategoria() == Vehicle.LUXE &&
-                lloguer.getDies()>1 ) {
+        if (getVehicle().getCategoria() == Vehicle.LUXE &&
+                getDies()>1 ) {
             bonificacions ++;
         }
         return bonificacions;
