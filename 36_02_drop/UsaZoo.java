cb59d2485f58050b4f50eb40d563dd3c62bff233
Soufian El Hajouji El Hayadi <proni10102002@gmail.com>
2023-05-23 18:17:21 +0200

    fet

---
+++ 36_02_drop/UsaZoo.java
@@ -0,0 +1,64 @@
+/*
+ * el programa s'encarrega de connectar a una base de dades i fer operacions 
+ * relacionades amb la creació i eliminació d'una taula anomenada "CATEGORIES" 
+ * en aquesta base de dades.
+ *
+ */
+
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+public class UsaZoo {
+    public static void main( String args[]) throws SQLException {
+        Zoo zoo = new Zoo();
+        System.out.print("Primer connectem amb la base de dades: ");
+        zoo.connecta();
+        System.out.println("connectat");
+
+        System.out.println("Eliminem la taula CATEGORIES per si existia");
+        zoo.eliminaTaulaCategories();
+
+        System.out.println("Creem la taula CATEGORIES (primer cop)");
+        zoo.creaTaulaCategories();
+        System.out.println("Eliminem la taula CATEGORIES");
+        zoo.eliminaTaulaCategories();
+
+        System.out.println("Creem la taula CATEGORIES (segon cop)");
+        zoo.creaTaulaCategories();
+        System.out.println("Creem la taula CATEGORIES (tercer cop)");
+        zoo.creaTaulaCategories();
+
+        System.out.print("Finalment tanquem la connexió amb la base de dades: ");
+        zoo.desconnecta();
+        System.out.println("desconnectat");
+    }
+    public void creaTaulaCategories() throws SQLException {
+        String sql = "CREATE TABLE  CATEGORIES (" +
+                     "       id        INTEGER PRIMARY KEY AUTOINCREMENT," +
+                     "       nom       VARCHAR(40))";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(sql);
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+    public void eliminaTaulaCategories() throws SQLException {
+        String sql = "DROP TABLE IF EXISTS CATEGORIES;";
+        //Statement st = null;
+       // try {
+        //    st = conn.createStatement();
+            st.executeUpdate(sql);
+      //  } finally {
+      //      if (st != null) {
+      //          st.close();
+     //       }
+        }
+    }
+    
+    
+}
