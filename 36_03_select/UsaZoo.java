139c55d135a23ecb577a45741d4667460dc48203
Soufian El Hajouji El Hayadi <proni10102002@gmail.com>
2023-05-24 17:21:29 +0200

    fet

---
+++ 36_03_select/UsaZoo.java
@@ -0,0 +1,48 @@
+/*
+ * el programa s'encarrega de connectar a una base de dades i fer operacions 
+ * relacionades amb la creació i eliminació d'una taula anomenada "CATEGORIES" 
+ * en aquesta base de dades.
+ *
+ */
+
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class UsaZoo {
+    public static void main( String args[]) throws SQLException {
+        Zoo zoo = new Zoo();
+        System.out.print("Primer connectem amb la base de dades: ");
+        zoo.connecta();
+        System.out.println("connectat");
+
+        System.out.println("Creem la taula CATEGORIES");
+        zoo.creaTaulaCategories();
+        System.out.println();
+        System.out.println("Mostrem les categories disponibles inicialment");
+        ZooUtils.mostraCategories(zoo.recuperaCategories());
+
+        Categoria ocell = new Categoria("ocell");
+        Categoria mamifer = new Categoria("mamífer");
+        System.out.println();
+        System.out.println("Inserim les categories: ");
+        System.out.println("\t"+ ocell);
+        System.out.println("\t"+ mamifer);
+        zoo.afegeixCategoria(ocell);
+        zoo.afegeixCategoria(mamifer);
+
+        System.out.println()
+        System.out.println("Mostrem les categories disponibles ara");
+        ZooUtils.mostraCategories(zoo.recuperaCategories());
+        System.out.println();
+        System.out.println("Consultem algunes categories");
+        System.out.println("En consultar per la categoria ocell obtenim " + zoo.obteCategoriaPerNom("ocell"));
+        System.out.println("En consultar per la categoria mamífer obtenim " + zoo.obteCategoriaPerNom("mamífer"));
+        System.out.println("En consultar per la categoria peix obtenim " + zoo.obteCategoriaPerNom("peix"));
+
+        System.out.print("Finalment tanquem la connexió amb la base de dades: ");
+        zoo.desconnecta();
+        System.out.println("desconnectat");
+    }
+}
---
+++ 36_03_select/Zoo.java
@@ -0,0 +1,71 @@
+/*
+ * el programa s'encarrega de connectar a una base de dades i fer operacions 
+ * relacionades amb la creació i eliminació d'una taula anomenada "CATEGORIES" 
+ * en aquesta base de dades.
+ *
+ */
+ 
+ 
+ 
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+public class Zoo {
+    private static final String NOM_BASE_DE_DADES = "animals.bd";
+    private static final String CADENA_DE_CONNEXIO = "jdbc:sqlite:" +
+                                                     NOM_BASE_DE_DADES;
+    private Connection conn = null;
+    public void connecta() throws SQLException {
+        if (conn != null) return;
+        conn = DriverManager.getConnection(CADENA_DE_CONNEXIO);
+    }
+
+    public void desconnecta() throws SQLException {
+        if (conn == null) return;
+        conn.close();
+        conn = null;
+    }
+        public void creaTaulaCategories() throws SQLException {
+        String sql = "DROP TABLE IF EXISTS CATEGORIES;"+
+                     "CREATE TABLE CATEGORIES (" +
+                     "       id        INTEGER PRIMARY KEY AUTOINCREMENT," +
+                     "       nom       VARCHAR(40))";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(sql);
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+    public void eliminaTaulaCategories() throws SQLException {
+        String sql = "DROP TABLE IF EXISTS CATEGORIES;";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(sql);
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+    
+    public void afegeixCategoria(Categoria categoria) throws SQLException {
+    String sql = String.format(
+            "INSERT INTO CATEGORIES (nom) VALUES ('%s')",
+            categoria.getNom());
+    Statement st = null;
+    try {
+        st = conn.createStatement();
+        st.executeUpdate(sql);
+    } finally {
+        if (st != null) {
+            st.close();
+        }
+    }
+}
+}
